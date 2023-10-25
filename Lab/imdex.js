ff849e38e3b971fa86835556da029ab43d28ad17
Ivan Pachev <mr.pachev@abv.bg>
2023-10-25 21:58:44 +0300

    Update

--- Lab/imdex.js
+++
@@ -1,7 +0,0 @@
-function printStudent(name, age, averageGrade) {
-  console.log(
-    `Name: ${name}, Age: ${age}, Average Grade: ${averageGrade.toFixed(2)}`
-  );
-}
-
-printStudent("ivan", 39, 4.7654);
