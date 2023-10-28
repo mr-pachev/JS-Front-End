72ea913d3d45063ebe7ed271addf9536f57b3be1
Ivan Pachev <mr.pachev@abv.bg>
2023-10-28 21:39:10 +0300

    Update

--- Exersice/vacation.js
+++ Exersice/vacation.js
@@ -13,8 +13,24 @@ function solve(countPeople, type, day){
             }
             break;
         case "Business":
+            if (day === "Friday"){
+                tottalPrice = countPeople * 10.90;
+            }else if (day === "Saturday"){
+                tottalPrice = countPeople * 15.60;
+            }else if (day === "Sunday"){
+                tottalPrice = countPeople * 16;
+            }
             break;
         case "Regular":
+            if (day === "Friday"){
+                tottalPrice = countPeople * 15;
+            }else if (day === "Saturday"){
+                tottalPrice = countPeople * 20;
+            }else if (day === "Sunday"){
+                tottalPrice = countPeople * 22.50;
+            }
             break;
+
+        
     }
 }
