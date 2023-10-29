fce6fd7a32ed41e32d01c5b8cf0dacda66ef0252
Ivan Pachev <mr.pachev@abv.bg>
2023-10-29 21:04:06 +0200

    Update

--- Exersice/leapYear.js
+++ Exersice/leapYear.js
@@ -1,9 +1,9 @@
-function solve(year){
-    if (year % 4 === 0 && year % 400 && year % 100 != 0){
-        console.log("yes");
-    }else {
-        console.log("no");
-    }
+function solve(year) {
+  if (year % 4 === 0 || (year % 400 != 0 && year % 100 === 0)) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
 }
 
-solve(2003);
+solve(1984);
