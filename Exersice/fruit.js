9ccfd6443893dbcdab5b2c8b79fd9117a4074c9d
Ivan Pachev <mr.pachev@abv.bg>
2023-10-30 22:04:32 +0200

    Update

--- Exersice/fruit.js
+++ Exersice/fruit.js
@@ -1,7 +1,11 @@
-function solve(fruit, currentWeight, pricePerKilo){
-    let gramsToKilos = currentWeight / 1000;
-    let neededSum = gramsToKilos * pricePerKilo;
-    console.log(`I need $${neededSum} to buy ${gramsToKilos.toFixed(2)} kilograms ${fruit}.`);
+function solve(fruit, currentWeight, pricePerKilo) {
+  let gramsToKilos = currentWeight / 1000;
+  let neededSum = gramsToKilos * pricePerKilo;
+  console.log(
+    `I need $${neededSum.toFixed(2)} to buy ${gramsToKilos.toFixed(
+      2
+    )} kilograms ${fruit}.`
+  );
 }
 
-solve('orange', 2500, 1.80);
+solve("orange", 2500, 1.8);
