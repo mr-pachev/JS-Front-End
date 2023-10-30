a7bbb434e5e7098d874b35a48226987897b8268f
Ivan Pachev <mr.pachev@abv.bg>
2023-10-30 22:03:42 +0200

    Update

--- Exersice/fruit.js
+++ Exersice/fruit.js
@@ -1,7 +1,7 @@
 function solve(fruit, currentWeight, pricePerKilo){
     let gramsToKilos = currentWeight / 1000;
     let neededSum = gramsToKilos * pricePerKilo;
-    console.log(`I need ${neededSum} to buy ${gramsToKilos} kilograms ${fruit}.`);
+    console.log(`I need $${neededSum} to buy ${gramsToKilos.toFixed(2)} kilograms ${fruit}.`);
 }
 
 solve('orange', 2500, 1.80);
