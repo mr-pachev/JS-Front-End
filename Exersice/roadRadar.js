8ba2389b97bc85915ac903929451915c8d326d43
Ivan Pachev <mr.pachev@abv.bg>
2023-10-31 17:59:28 +0200

    Update

---
+++ Exersice/roadRadar.js
@@ -0,0 +1,20 @@
+function solve(speed,area){
+    let statusSpeed = "";
+
+    switch(area){
+        case "residential":
+            if(speed === 20){
+                console.log(`Driving ${speed} km/h in a 20 zone`);
+            }
+            break;
+    
+        case "city":
+            break;
+    
+        case "interstate":
+            break;
+    
+        case "motorwayl":
+            break;
+    }
+}
