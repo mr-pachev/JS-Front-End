809f7ef18b38f706aa9cc1e96a31c7c8230036f3
Ivan Pachev <mr.pachev@abv.bg>
2023-11-02 13:44:25 +0200

    Update

--- Lab/JS Arrays & Strings/evenAndOddSubtraction.js
+++ Lab/JS Arrays & Strings/evenAndOddSubtraction.js
@@ -1,13 +1,15 @@
-function solve (argument){
-    let sumEven = 0;
-    let sumOdd = 0;
+function solve(argument) {
+  let sumEven = 0;
+  let sumOdd = 0;
 
-    for (let el of argument){
-        if (el % 2 == 0){
-            sumEven += el;
-        }else {
-            sumOdd += el;
-        }
+  for (let el of argument) {
+    if (el % 2 == 0) {
+      sumEven += el;
+    } else {
+      sumOdd += el;
     }
+  }
+
+  console.log(sumEven - sumOdd);
 }
-solve();
+solve([3, 5, 7, 9]);
