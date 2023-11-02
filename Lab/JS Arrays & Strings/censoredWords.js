015f6db7e76e26a5f5cd4d1d013cc14784e65b12
Ivan Pachev <mr.pachev@abv.bg>
2023-11-02 14:09:26 +0200

    Update

--- Lab/JS Arrays & Strings/censoredWords.js
+++ Lab/JS Arrays & Strings/censoredWords.js
@@ -1,5 +1,6 @@
-function solve(text, word){
-    
+function solve(text, word){;
+    text = text.replaceAll(word, '*'.repeat(word.length));
+    console.log(text);
 }
 
-solve ('A small sentence with some words', 'small');
+solve ('Find the hidden word', 'hidden');
