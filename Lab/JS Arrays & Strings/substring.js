c6bc3d8cb511113d7fb2247ae67f7e691fd1633b
Ivan Pachev <mr.pachev@abv.bg>
2023-11-02 14:02:04 +0200

    Update

--- Lab/JS Arrays & Strings/substring.js
+++ Lab/JS Arrays & Strings/substring.js
@@ -1,5 +1,5 @@
 function solve(argumen, startIndex, endIndex) {
-  //let sliceText = argumen.slice(startIndex, startIndex + endIndex);
+  //let text = argumen.slice(startIndex, startIndex + endIndex);
   let text = argumen.substring(startIndex, startIndex + endIndex);
 
   console.log(text);
