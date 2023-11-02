8ab3a86f7c1274353fc7cc481deed048f94d8728
Ivan Pachev <mr.pachev@abv.bg>
2023-11-02 14:38:04 +0200

    Update

--- Lab/JS Arrays & Strings/censoredWords.js
+++ Lab/JS Arrays & Strings/censoredWords.js
@@ -1,10 +1,15 @@
-function solve(text, word){
-    //text = text.replaceAll(word, '*'.repeat(word.length));
+function solve(text, word) {
+  //text = text.replaceAll(word, '*'.repeat(word.length));
 
-    while(text.includes(word)){
-        text = text.replace(word, '*'.repeat(word.length));
-    }
-    console.log(text);
+  // while(text.includes(word)){
+  //     text = text.replace(word, '*'.repeat(word.length));
+  // }
+  // console.log(text);
+
+
+  let regExp = new RegExp(`${word}`, 'g');
+  let replacement = "*".repeat(word.length);
+  console.log(text.replace(regExp, replacement));
 }
 
-solve ('Find hidden the hidden word', 'hidden');
+solve("Find hidden the hidden word", "hidden");
