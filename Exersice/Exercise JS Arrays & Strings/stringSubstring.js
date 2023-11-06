b191cb37bb211123055545f19fd00eb246b6b540
Ivan Pachev <mr.pachev@abv.bg>
2023-11-06 13:27:12 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/stringSubstring.js
+++ Exersice/Exercise JS Arrays & Strings/stringSubstring.js
@@ -1,14 +1,18 @@
 function solve(word, text){
 
-    let lowerLetters = text.toLowerCase();
+    let lowerLetters = text.toLowerCase().split(' ');  //превръщаме текстта в масив
+
+    let messge = `${word} not found!`;
    
-    if (lowerLetters.includes(word)){
-         console.log(word);
-    }else{
-        console.log(`${word} not found!`);
+    for (const el of lowerLetters) {
+        if (el === word){
+            messge = word;
+        }
     }
-  
+
+    console.log(messge);
 }
+
 
 solve('javascript',
 'JavaScript is the best programming language'
