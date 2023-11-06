56d504a341f4ab09e2829fdf0066040b12b1f400
Ivan Pachev <mr.pachev@abv.bg>
2023-11-06 12:04:59 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/revealWords.js
+++ Exersice/Exercise JS Arrays & Strings/revealWords.js
@@ -1,20 +1,35 @@
 function solve (words, text){
     let textArr = text.split(' ');
+    let wordsArr = words.split(', ');
     let hiddenWordsArr = [];
 
     for (let i = 0; i < textArr.length; i++) {          //намираме индексите на скритите думи
-        if (textArr[i].incuse('*')){
+        if (textArr[i].includes('*')){
             hiddenWordsArr.push(i);
         }
     }
 
+    let arr = String(textArr);
+
     for (let j = 0; j < hiddenWordsArr.length; j++){     //въртим цикъл според скритите думи
-        for(let k = 0; k < words.lenght; k++){
-            if (currentHiddenWord.length == )
+        
+        for (let k = 0; k < wordsArr.length; k++){
+
+
+            let hiddent = textArr[hiddenWordsArr[j]];
+            let currentWord =  wordsArr[k];
+            
+
+            if (hiddent.length === currentWord.length){
+                arr.replace(hiddent, currentWord);
+            }
 
         }
-            
+          
     }
+     console.log(textArr);
 }
 
-solve(words, text);
+solve('great, learning',
+'softuni is ***** place for ******** new programming languages'
+);
