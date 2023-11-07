16a35e1cb8f7b99c70ff0a5653e905eef3c0d390
Ivan Pachev <mr.pachev@abv.bg>
2023-11-07 16:49:23 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/login.js
+++ Exersice/Exercise JS Arrays & Strings/login.js
@@ -1,23 +1,22 @@
-function solve(input){
-    let username = input[0].split('');
-    let usernamePass = String(username.reverse().join(''));
-    username = input[0];
-    let atteptCount = 0;
+function solve(input) {
+  let username = input[0].split("");
+  let usernamePass = String(username.reverse().join(""));
+  username = input[0];
+  let atteptCount = 1;
 
-    for (const attempt of input) {
-
-        if (attempt === usernamePass && atteptCount <= 4){
-            console.log(`User ${username} logged in.`);
-            break;
-        }else if (atteptCount < 4){
-            console.log('Incorrect password. Try again.');
-        }else {
-            console.log(`User ${username} blocked!`);
-            break;
-        }
-
-        atteptCount++;
+  for (let index = 1; index < input.length; index++) {
+    if (input[index] === usernamePass && atteptCount <= 4) {
+      console.log(`User ${username} logged in.`);
+      break;
+    } else if (atteptCount < 4) {
+      console.log("Incorrect password. Try again.");
+    } else {
+      console.log(`User ${username} blocked!`);
+      break;
     }
+
+    atteptCount++;
+  }
 }
 
-solve(['sunny','rainy','cloudy','sunny','not sunny']);
+solve(["sunny", "rainy", "cloudy", "sunny", "not sunny"]);
