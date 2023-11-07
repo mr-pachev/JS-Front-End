80ee8db3252f46559f74b3c248fe4dbb63b59b54
Ivan Pachev <mr.pachev@abv.bg>
2023-11-07 16:32:39 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/login.js
+++ Exersice/Exercise JS Arrays & Strings/login.js
@@ -1,9 +1,13 @@
 function solve(input){
-    let usernameArr = input.split(' ');
-    let username = 
+    let username = input[0].split('');
+    let usernamePass = String(username.reverse().join(''));
     let atteptCount = 1;
 
-    for (const attempt of usernameArr) {
-        
+    for (const attempt of input) {
+        if (attempt === usernamePass){
+            console.log(``);
+        }
     }
 }
+
+solve(['Acer','login','go','let me in','recA']);F
