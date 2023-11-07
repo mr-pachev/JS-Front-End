98c4c107fa7eca481f03a76ff35cafd34a240281
Ivan Pachev <mr.pachev@abv.bg>
2023-11-07 16:40:29 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/login.js
+++ Exersice/Exercise JS Arrays & Strings/login.js
@@ -1,12 +1,21 @@
 function solve(input){
     let username = input[0].split('');
     let usernamePass = String(username.reverse().join(''));
+    username = input[0];
     let atteptCount = 1;
 
     for (const attempt of input) {
-        if (attempt === usernamePass){
-            console.log(``);
+
+        if (attempt === usernamePass && atteptCount < 4){
+            console.log(`User ${username} logged in.`);
+        }else if (atteptCount < 4){
+            console.log('Incorrect password. Try again.');
+        }else {
+            console.log(`User ${username} blocked!`);
+            break;
         }
+
+        atteptCount++;
     }
 }
 
