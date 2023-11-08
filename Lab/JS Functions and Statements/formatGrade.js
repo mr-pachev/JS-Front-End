76ed8c80d3de115896468d2c1347b7e43472b53f
Ivan Pachev <mr.pachev@abv.bg>
2023-11-08 21:38:04 +0200

    Update

--- Lab/JS Functions and Statements/formatGrade.js
+++ Lab/JS Functions and Statements/formatGrade.js
@@ -1,15 +1,16 @@
 function formatGrade(grade) {
+  
   if (grade < 3.0) {
-    console.log("Fail");
+    console.log(`Fail (${grade})`);
   } else if (grade < 3.5) {
-    console.log("Poor");
+    console.log(`Poor (${grade.toFixed(2)})`);
   } else if (grade < 4.5) {
-    console.log("Good");
+    console.log(`Good (${grade.toFixed(2)})`);
   } else if (grade < 5.5) {
-    console.log("Very good");
+    console.log(`Very good (${grade.toFixed(2)})`);
   } else if (grade >= 5.5) {
-    console.log("Excellent");
+    console.log(`Excellent (${grade.toFixed(2)})`);
   }
 }
 
-formatGrade(3.33);
+formatGrade(2.00);
