b57db1a59174da23ec9aa95b194023f36983aed4
Ivan Pachev <mr.pachev@abv.bg>
2023-11-08 20:14:03 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/revealWords.js
+++ Exersice/Exercise JS Arrays & Strings/revealWords.js
@@ -1,31 +1,28 @@
-function solve (words, text){
-    let textArr = text.split(' ');
-    let wordsArr = words.split(', ');
-    let hiddenWordsArr = [];
+function solve(words, text) {
+  let textArr = text.split(" ");
+  let wordsArr = words.split(", ");
+  let hiddenWordsArr = [];
 
-    for (let i = 0; i < textArr.length; i++) {          //намираме индексите на скритите думи
-        if (textArr[i].includes('*')){
-            hiddenWordsArr.push(i);
-        }
+  for (let i = 0; i < textArr.length; i++) {
+    //намираме индексите на скритите думи
+    if (textArr[i].includes("*")) {
+      hiddenWordsArr.push(i);
     }
+  }
 
-    for (let j = 0; j < hiddenWordsArr.length; j++){     //въртим цикъл според скритите думи
-        for (let k = 0; k < wordsArr.length; k++){
+  for (let j = 0; j < hiddenWordsArr.length; j++) {
+    //въртим цикъл според скритите думи
+    for (let k = 0; k < wordsArr.length; k++) {
+      let hiddent = textArr[hiddenWordsArr[j]];
+      let currentWord = wordsArr[k];
 
-            let hiddent = textArr[hiddenWordsArr[j]];
-            let currentWord =  wordsArr[k];
-            
-            if (hiddent.length === currentWord.length){
-                let indexHiddenWords = hiddenWordsArr[j];
-               textArr[indexHiddenWords] = currentWord;
-            }
-
-        }
-          
+      if (hiddent.length === currentWord.length) {
+        let indexHiddenWords = hiddenWordsArr[j];
+        textArr[indexHiddenWords] = currentWord;
+      }
     }
-     console.log(textArr.join(' '));
+  }
+  console.log(textArr.join(" "));
 }
 
-solve('great',
-'softuni is ***** place for learning new programming languages'
-);
+solve("great", "softuni is ***** place for learning new programming languages");
