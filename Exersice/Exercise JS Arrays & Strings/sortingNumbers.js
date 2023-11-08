6f8db4775304e3aab4464ac841ed5000875bb8e9
Ivan Pachev <mr.pachev@abv.bg>
2023-11-08 18:56:43 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/sortingNumbers.js
+++ Exersice/Exercise JS Arrays & Strings/sortingNumbers.js
@@ -1,15 +1,15 @@
-function solve(numsArr){
-    let resultArr = [];
-    numsArr.sort((a,b) => a - b);
+function solve(numsArr) {
+  let resultArr = [];
+  numsArr.sort((a, b) => a - b);
 
-    while (numsArr.length !== 0) {
-        let firstNum = numsArr.shift();
-        let secondNum = numsArr.pop();
+  while (numsArr.length !== 0) {
+    let firstNum = numsArr.shift();
+    let secondNum = numsArr.pop();
 
-        resultArr.push(firstNum);
-        resultArr.push(secondNum);
-    }
-    return resultArr;
+    resultArr.push(firstNum);
+    resultArr.push(secondNum);
+  }
+  return resultArr;
 }
 
 solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
