daee15dd3e64b3cead9cdcf1bc2a2f1b0b6cfe62
Ivan Pachev <mr.pachev@abv.bg>
2023-11-08 20:14:37 +0200

    Update

--- Exersice/Exercise JS Arrays & Strings/printEveryNthElementFromAnArray.js
+++ Exersice/Exercise JS Arrays & Strings/printEveryNthElementFromAnArray.js
@@ -1,17 +1,11 @@
-function solve(arr, n){
-    let newArr = [];
+function solve(arr, n) {
+  let newArr = [];
 
-    for (let i = 0; i < arr.length; i += n){
-        let curren = arr[i];
-        newArr.push(arr[i]);
-    }
-    return newArr;
-    }
+  for (let i = 0; i < arr.length; i += n) {
+    let curren = arr[i];
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
 
-solve(['dsa',
-'asd', 
-'test', 
-'tset'], 
-2
-
-);
+solve(["dsa", "asd", "test", "tset"], 2);
