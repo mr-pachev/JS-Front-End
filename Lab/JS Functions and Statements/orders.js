27caf72bb82ded68ff4ff6df71b4770790afa509
Ivan Pachev <mr.pachev@abv.bg>
2023-11-09 16:03:51 +0200

    Update

--- Lab/JS Functions and Statements/orders.js
+++ Lab/JS Functions and Statements/orders.js
@@ -1,9 +1,15 @@
-function orders(product, quantity){
-    cost products = {
-        coffee = 1.50,
-water: 1.00,
-coke: 1.40,
-snacks: 2.00
+function orders(product, quantity) {
+  const products = {
+    coffee: 1.5,
+    water: 1.0,
+    coke: 1.4,
+    snacks: 2.0,
+  };
 
-    }
+  const productValue = products[product];
+  const price = quantity * productValue;
+
+  console.log(price.toFixed(2));
 }
+
+orders("coffee", 2);
