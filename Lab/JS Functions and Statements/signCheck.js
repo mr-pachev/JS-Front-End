1a35cc108dc0e7ffa969462c39e5ab7982f0eb25
Ivan Pachev <mr.pachev@abv.bg>
2023-11-09 16:53:19 +0200

    Update

--- Lab/JS Functions and Statements/signCheck.js
+++ Lab/JS Functions and Statements/signCheck.js
@@ -1,7 +1,24 @@
 function signCheck(firstNum, secondNum, thridNum) {
-  if (firstNum * secondNum * thridNum > 0) {
+  //   if (firstNum * secondNum * thridNum > 0) {
+  //     console.log("Positive");
+  //   } else {
+  //     console.log("Negative");
+  //   }
+
+  if (
+    (firstNum > 0 && secondNum > 0 && thridNum > 0) ||
+    (firstNum < 0 && secondNum < 0 && thridNum > 0) ||
+    (firstNum > 0 && secondNum < 0 && thridNum < 0) ||
+    (firstNum < 0 && secondNum > 0 && thridNum < 0)
+  ) {
     console.log("Positive");
-  } else {
+  } else if (
+    (firstNum > 0 && secondNum > 0 && thridNum > 0) ||
+    (firstNum < 0 && secondNum > 0 && thridNum > 0) ||
+    (firstNum > 0 && secondNum < 0 && thridNum > 0) ||
+    (firstNum > 0 && secondNum > 0 && thridNum < 0) ||
+    (firstNum < 0 && secondNum < 0 && thridNum < 0)
+  ) {
     console.log("Negative");
   }
 }
