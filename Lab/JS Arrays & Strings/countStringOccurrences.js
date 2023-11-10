73770e850a580cbf43121b04e61a3340b7f3372f
Ivan Pachev <mr.pachev@abv.bg>
2023-11-10 21:46:30 +0200

    Update

--- Lab/JS Arrays & Strings/countStringOccurrences.js
+++ Lab/JS Arrays & Strings/countStringOccurrences.js
@@ -1,19 +1,19 @@
 function solve(text, word) {
-//   const regex = new RegExp(`\\b${word}\\b`, "g");
-//   const matches = text.match(regex);
+  //   const regex = new RegExp(`\\b${word}\\b`, "g");
+  //   const matches = text.match(regex);
 
-//   console.log(matches.length);
+  //   console.log(matches.length);
 
-let textNoSpice = text.split(' ');
-let matches = 0;
+  let textNoSpice = text.split(" ");
+  let matches = 0;
 
-for(let el of textNoSpice){
-    if (el === `${word}`){
-        matches++;
+  for (let el of textNoSpice) {
+    if (el === `${word}`) {
+      matches++;
     }
-}
+  }
 
-console.log(matches);
+  console.log(matches);
 }
 
 solve("This is a word and it also is a sentence", "is");
