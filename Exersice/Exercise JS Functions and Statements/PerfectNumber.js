18368da60d8270dabdc83fdf60dabd22d55bc720
Ivan Pachev <mr.pachev@abv.bg>
2023-11-11 18:53:11 +0200

    Update

--- Exersice/Exercise JS Functions and Statements/PerfectNumber.js
+++ Exersice/Exercise JS Functions and Statements/PerfectNumber.js
@@ -1,12 +1,16 @@
 function perfectNumber(num) {
   let sum = 0;
-  for (let i = 1; i < num; i++) {
-    if (num % i == 0) {
-      sum += i;
+
+  function sumDivisors(num) {
+    for (let i = 1; i < num; i++) {
+      if (num % i == 0) {
+        sum += i;
+      }
     }
+    return sum;
   }
 
-  if (sum === num) {
+  if (sumDivisors(num) === num) {
     console.log("We have a perfect number!");
   } else {
     console.log("It's not so perfect.");
