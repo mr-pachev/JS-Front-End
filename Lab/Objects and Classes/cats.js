ec064ac386cdbf13b141d561ad91534306689c4e
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 12:59:19 +0200

    Update

---
+++ Lab/Objects and Classes/cats.js
@@ -0,0 +1,13 @@
+function solve(input){
+
+    for (const line of input){
+        let name = line.split(' ')[0];
+        let age = line.split(' ')[1];
+        age = Number(age);
+
+        
+    }
+    
+}
+
+solve(['Mellow 2', 'Tom 5']);
