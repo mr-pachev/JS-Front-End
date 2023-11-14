978ac8faa679434592e66d7b80eac41eee75ceda
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 13:03:47 +0200

    Update

--- Lab/Objects and Classes/cats.js
+++ Lab/Objects and Classes/cats.js
@@ -1,13 +1,19 @@
-function solve(input){
-
-    for (const line of input){
-        let name = line.split(' ')[0];
-        let age = line.split(' ')[1];
-        age = Number(age);
-
-        
+function solve(input) {
+   class Cat {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
     }
-    
+    meow() {
+      console.log(`${name}, age ${age} says Meow`);
+    }
+  }
+
+  for (const line of input) {
+    let name = line.split(" ")[0];
+    let age = line.split(" ")[1];
+    age = Number(age);
+  }
 }
 
-solve(['Mellow 2', 'Tom 5']);
+solve(["Mellow 2", "Tom 5"]);
