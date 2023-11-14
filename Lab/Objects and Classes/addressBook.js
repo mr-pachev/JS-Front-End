4e31b172e31482d8e2d1f1e8765f8e4cbeddebf9
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 12:49:56 +0200

    Update

--- Lab/Objects and Classes/addressBook.js
+++ Lab/Objects and Classes/addressBook.js
@@ -1,11 +1,24 @@
 function solve(input) {
-  let adressBook = {};
+  let addressBook = {};
 
   for (const line of input) {
     let name = line.split(":")[0];
-    let adress = line.split(":")[1];
+    let address = line.split(":")[1];
 
-    adressBook[name] = adress;
+    addressBook[name] = address;
+  }
+
+  let entries = Object.entries(addressBook); //превръщане на обекта people в масив от масиви
+
+  let sortedByName = entries.sort((personA, personB) => {
+    let personAName = personA[0];
+    let personBName = personB[0];
+
+    return personAName.localeCompare(personBName);
+  });
+
+  for (const el of sortedByName) {
+    console.log(`${el[0]} -> ${el[1]}`);
   }
 }
 
