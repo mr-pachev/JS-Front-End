2a049941f63d8de31d561ba9387e82e682655aca
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 11:44:47 +0200

    Update

--- Lab/Objects and Classes/phoneBook.js
+++ Lab/Objects and Classes/phoneBook.js
@@ -1,9 +1,19 @@
-function solve(input){
-    let phoneBook = {
-        for (const el of input) {
-         let key = el.split(' ')[0];
-         let valuw = el.split(' ')[1];
-         phoneBook.
-        }
-    }
+function solve(input) {
+  let phoneBook = {};
+
+  for (const iterator of input) {
+    let name = iterator.split(" ")[0];
+    let number = iterator.split(" ")[1];
+
+    phoneBook[name] = number;
+
+    console.log(name);
+  }
 }
+
+solve([
+  "Tim 0834212554",
+  "Peter 0877547887",
+  "Bill 0896543112",
+  "Tim 0876566344",
+]);
