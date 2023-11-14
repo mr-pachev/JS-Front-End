0dca16430485ef3805fc2f30f19593fda107afba
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 13:28:48 +0200

    Update

--- Lab/Objects and Classes/cats.js
+++ Lab/Objects and Classes/cats.js
@@ -1,19 +1,22 @@
 function solve(input) {
-   class Cat {
+  class Cat {                           //създаване на клас
     constructor(name, age) {
       this.name = name;
       this.age = age;
     }
-    meow() {
-      console.log(`${name}, age ${age} says Meow`);
+    meow() {                            //метод за мяучане
+      console.log(`${this.name}, age ${this.age} says Meow`);
     }
   }
 
-  for (const line of input) {
+  for (const line of input) {           //обхождане на васива
     let name = line.split(" ")[0];
     let age = line.split(" ")[1];
     age = Number(age);
+
+    let currentCat = new Cat(name, age); //създаване на конструктор 
+    currentCat.meow();                  //извикване на метода от създадения клас
   }
 }
 
-solve(["Mellow 2", "Tom 5"]);
+solve(["Candy 1", "Poppy 3", "Nyx 2"]);
