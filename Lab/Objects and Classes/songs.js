74ddb725dff9f85ae33f7e4e0a2e7fcb363acb1c
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 13:55:21 +0200

    Update

--- Lab/Objects and Classes/songs.js
+++ Lab/Objects and Classes/songs.js
@@ -1,22 +1,37 @@
-function solve(input){
-    class Song {
-        constructor(typeList, name, time){
-            this.typeList = typeList;
-            this.name = name;
-            this.time = time;
-        }
-
-        print(){
-            console.log(`${this.name}`);
-        }
+function solve(input) {
+  class Song {
+    constructor(typeList, name, time) {
+      this.typeList = typeList;
+      this.name = name;
+      this.time = time;
     }
 
-    
+    print() {
+      console.log(`${this.name}`);
+    }
+  }
+
+  let countSong = input[0];
+  let typeSong = input[input.length - 1];
+
+  for (let el = 1; el <= countSong; el++) {
+    let song = input[el];
+    let typeList = song.split('_')[0];
+    let name = song.split('_')[1];
+    let time = song.split('_')[2];
+
+    let currentSong = new Song(typeList, name, time);
+
+    if (countSong.typeList === typeSong) {
+      currentSong.print();
+    }
+  }
 }
 
-solve([3,
-    'favourite_DownTown_3:14',
-    'favourite_Kiss_4:16',
-    'favourite_Smooth Criminal_4:01',
-    'favourite']
-    );
+solve([
+  3,
+  "favourite_DownTown_3:14",
+  "favourite_Kiss_4:16",
+  "favourite_Smooth Criminal_4:01",
+  "favourite",
+]);
