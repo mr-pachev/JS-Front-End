1fb89fad9872f343241fe7d7fba8c63512c6ccfb
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 13:39:01 +0200

    Update

---
+++ Lab/Objects and Classes/songs.js
@@ -0,0 +1,22 @@
+function solve(input){
+    class Song {
+        constructor(typeList, name, time){
+            this.typeList = typeList;
+            this.name = name;
+            this.time = time;
+        }
+
+        print(){
+            console.log(`${this.name}`);
+        }
+    }
+
+    
+}
+
+solve([3,
+    'favourite_DownTown_3:14',
+    'favourite_Kiss_4:16',
+    'favourite_Smooth Criminal_4:01',
+    'favourite']
+    );
