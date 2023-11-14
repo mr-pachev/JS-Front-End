c0b6c37726e0d1fe7c5f8a4f6408ae9a57ec2112
Ivan Pachev <mr.pachev@abv.bg>
2023-11-14 11:25:19 +0200

    Update

---
+++ Lab/Objects and Classes/phoneBook.js
@@ -0,0 +1,9 @@
+function solve(input){
+    let phoneBook = {
+        for (const el of input) {
+         let key = el.split(' ')[0];
+         let valuw = el.split(' ')[1];
+         phoneBook.
+        }
+    }
+}
