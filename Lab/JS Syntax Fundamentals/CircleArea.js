14726e5a790e69867def7217023abbbc83fab826
Ivan Pachev <mr.pachev@abv.bg>
2023-11-17 21:37:51 +0200

    Update

--- Lab/JS Syntax Fundamentals/CircleArea.js
+++ Lab/JS Syntax Fundamentals/CircleArea.js
@@ -1,13 +1,14 @@
-function solved(input){
-    
-    let param = typeof(input);   //записва типа на променливата
+function solved(input) {
+  let param = typeof input; //записва типа на променливата
 
-    if (param === 'number'){
-        let result = Math.pow(input, 2) * Math.PI;
-        console.log(result.toFixed(2));
-    }else{
-        console.log(`We can not calculate the circle area, because we receive a ${param}.`);
-    }
+  if (param === "number") {
+    let result = Math.pow(input, 2) * Math.PI;
+    console.log(result.toFixed(2));
+  } else {
+    console.log(
+      `We can not calculate the circle area, because we receive a ${param}.`
+    );
+  }
 }
 
 solved(5);
