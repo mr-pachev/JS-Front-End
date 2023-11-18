23d11f1fba57b0661fe5d059d46e9b10e7f57d38
Ivan Pachev <mr.pachev@abv.bg>
2023-11-18 17:25:55 +0200

    Update

---
+++ Exersice/Exercise Objects and Classes/еmployees.js
@@ -0,0 +1,12 @@
+function solve(input) {
+  for (const current of input) {
+    console.log(`Name: ${current} -- Personal Number: ${current}`);
+  }
+}
+
+solve([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
