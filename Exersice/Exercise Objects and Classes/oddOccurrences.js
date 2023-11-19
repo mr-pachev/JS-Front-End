ad18ec9d0f2ba8e4a8b565170f03bddbff44e8c7
Ivan Pachev <mr.pachev@abv.bg>
2023-11-19 18:50:27 +0200

    Update

--- Exersice/Exercise Objects and Classes/oddOccurrences.js
+++ Exersice/Exercise Objects and Classes/oddOccurrences.js
@@ -3,18 +3,16 @@ function solve(input) {
 
   let wordsArr = [];
 
-  for (const iterator of inputArr) {        //пълнене на масива с обекти (дума срещана по един път)
+  for (const iterator of inputArr) {    //пълнене на масива с обекти (дума срещана по един път)
     let search = wordsArr.find((el) => el.name === iterator);
     if (!search) {
       wordsArr.push({ name: iterator, count: 0 });
     }
   }
 
-  for (const obj of wordsArr) {             //обхождане на масива с обекти
-    console.log(obj.name);
+  for (const obj of wordsArr) {         //обхождане на масива с обекти
 
-    for (const word of inputArr) {          //бохождане на думите от входа
-      console.log(word);
+    for (const word of inputArr) {      //бохождане на думите от входа
 
       if (obj.name === word) {
         obj.count += 1;
@@ -22,7 +20,19 @@ function solve(input) {
     }
   }
 
-  console.log();
+  for (let i = 0; i < wordsArr.length; i++) {
+    if (wordsArr[i].count % 2 === 0) {
+      wordsArr.splice(i, 1);
+      i--;
+    }
+  }
+
+  let result = '';
+ for (const iterator of wordsArr) {
+    result += iterator.name + ' ';
+ }
+
+ console.log(result);
 }
 
-solve("Java C# Php PHP Java PhP 3 C# 3 1 5 C#");
+solve('Cake IS SWEET is Soft CAKE sweet Food');
