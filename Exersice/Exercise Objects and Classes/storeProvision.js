59b2586773c3257784d80399981f44df2c68dc7b
Ivan Pachev <mr.pachev@abv.bg>
2023-11-19 11:54:52 +0200

    Update

---
+++ Exersice/Exercise Objects and Classes/storeProvision.js
@@ -0,0 +1,6 @@
+function solve() {}
+
+solve(
+  ["Chips", "5", "CocaCola", "9", "Bananas", "14", "Pasta", "4", "Beer", "2"],
+  ["Flour", "44", "Oil", "12", "Pasta", "7", "Tomatoes", "70", "Bananas", "30"]
+);
