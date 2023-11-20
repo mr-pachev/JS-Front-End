94d7e87fbde8ee3ad81840ace4343074d9f2dac5
Ivan Pachev <mr.pachev@abv.bg>
2023-11-20 12:29:53 +0200

    Update

--- Exersice/Exercise Objects and Classes/makeADictionary.js
+++ Exersice/Exercise Objects and Classes/makeADictionary.js
@@ -3,6 +3,29 @@ function solve(input) {
 
   for (const iterator of input) {
     let object = JSON.parse(iterator); //конвертиране от JSON в Obj
+
+    for (const el of descriptionArr) {
+        for (const keyy in el) {
+            
+            function areObjectsEqual(keyy, obj) {
+                // Проверка за броя на ключовете
+                if (Object.keys(keyy).length !== Object.keys(obj).length) {
+                  return false;
+                }
+                          
+                return true;
+              }
+
+
+            if (areObjectsEqual){
+                let index = descriptionArr.indexOf(el);
+	descriptionArr.splice(index, 1);
+            };
+    }
+    
+    }   
+
+
     descriptionArr.push(object);
   }
 
@@ -21,7 +44,7 @@ function solve(input) {
       } else if (keyA > keyB) {
         return 1;
       }
-    }
+    };
 
     // Ако ключовете са еднакви, сравни стойностите
     return 0;
@@ -37,6 +60,7 @@ function solve(input) {
 solve([
   '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
   '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+  '{"Coffee":"Bla Bla."}',
   '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
   '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
   '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
