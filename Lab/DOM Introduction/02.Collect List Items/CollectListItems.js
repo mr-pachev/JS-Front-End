a583acd7d712641fb8bbdd7d55f928c3e7beeb79
Ivan Pachev <mr.pachev@abv.bg>
2023-11-23 17:27:14 +0200

    Update

--- Lab/DOM Introduction/02.Collect List Items/CollectListItems.js
+++ Lab/DOM Introduction/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+  console.log("here");
+  const list = document.getElementsByTagName("li");
+  const elementsAsArray = Array.from(list);
+
+  for (const el of elementsAsArray) {
+    console.log(el);
+  }
 }
