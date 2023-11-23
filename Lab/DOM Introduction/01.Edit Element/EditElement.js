bb8689b5a9074c77a70acbea609624db5aba9e33
Ivan Pachev <mr.pachev@abv.bg>
2023-11-23 14:34:54 +0200

    Update

--- Lab/DOM Introduction/01.Edit Element/EditElement.js
+++ Lab/DOM Introduction/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer) {
+  let content = ref.textContent;
+  let matcher = new RegExp(match, g);
+  let edited = content.replace(match, replacer);
 }
