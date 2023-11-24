994e6ab1575a02f7aa1c991b434289f7d9e4e8ad
Ivan Pachev <mr.pachev@abv.bg>
2023-11-24 12:07:14 +0200

    Update

--- Lab/DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ Lab/DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
+  const con = document.getElementById("content").textContent;       //взима съдържанието на тага с id=content
+  const reg = /\((.+?)\)/g;
 
+  let matching = reg.exec(con);
+  let matchingText = "";
+
+  while (matching !== null) {                                       //проверка докато има съвпадение
+    matchingText += matching[1] + "; ";
+    matching = reg.exec(con);                                       //прилагане на проверка за всяка часто от текста
+  }
+  return (document.getElementById("content").textContent = matchingText);
 }
