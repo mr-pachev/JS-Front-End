a2dbdee139407864848c12e088685bf0d71dc8c7
Ivan Pachev <mr.pachev@abv.bg>
2023-11-24 10:43:08 +0200

    Update

--- Lab/DOM Introduction/06.Sum Table/SumTable.js
+++ Lab/DOM Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,8 @@
 function sumTable() {
+  const nodeList = document.querySelectorAll("tbody tr"); //редовете от таблицата
 
+  for (let index = 1; index < nodeList.length; index++) {
+    const tdArrInCurrentRow = nodeList[index].children;
+    console.log(tdArrInCurrentRow[1]);
+  }
 }
