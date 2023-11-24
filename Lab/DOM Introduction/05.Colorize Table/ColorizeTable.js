5015e77f7a120578aa671bbf2149cc44a4cb8ca2
Ivan Pachev <mr.pachev@abv.bg>
2023-11-24 09:54:14 +0200

    Update

--- Lab/DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ Lab/DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+  const nodeList = document.querySelectorAll("tr");
+  const listToArr = Array.from(nodeList);
+  
+  for (let index = 0; index < listToArr.length; index++) {
+    console.log(listToArr[index]);
+  }
 }
