79f1b3728def7607e62341b56e0f18e70956663c
Ivan Pachev <mr.pachev@abv.bg>
2023-11-24 11:03:38 +0200

    Update

--- Lab/DOM Introduction/06.Sum Table/SumTable.js
+++ Lab/DOM Introduction/06.Sum Table/SumTable.js
@@ -1,10 +1,10 @@
 function sumTable() {
-  const nodeList = document.querySelectorAll("table tr"); //редовете от таблицата
+  const nodeList = document.querySelectorAll("table tr");       //редовете от таблицата
 
   let sum = 0;
   for (let index = 1; index < nodeList.length; index++) {
-    const tdInCurrentRow = nodeList[index].children;
-    const currentSum = Number(tdInCurrentRow[1].textContent);
+    const tdInCurrentRow = nodeList[index].children;            //децата на всеки tr
+    const currentSum = Number(tdInCurrentRow[1].textContent);   //съдържанието на последния td и превръщането му в число
     sum += currentSum;
   }
 
