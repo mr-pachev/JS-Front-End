f7d072be13081ded0743bdfadf35f6b74ebb57d8
Ivan Pachev <mr.pachev@abv.bg>
2023-11-25 11:26:21 +0200

    Update

--- Exersice/Exercise DOM Introduction/02. Pascal or Camel Case/solution.js
+++ Exersice/Exercise DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -1,5 +1,9 @@
 function solve() {
-  let textInput = document.getElementById("text").value;
+  let text = document.getElementById("text").value;
   let namingConvention = document.getElementById("naming-convention").value;
   let transformText = document.getElementById("Transform").value;
+
+  let textArr = input.split('');
+
+  console.log(input);
 }
