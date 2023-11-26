eab2bd1aa37bec1819342733b668199996a0b205
Ivan Pachev <mr.pachev@abv.bg>
2023-11-26 11:38:51 +0200

    Update

--- Exersice/Exercise DOM Introduction/04. Search in List/search.js
+++ Exersice/Exercise DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,12 @@
 function search() {
-   // TODO
+  let arrWords = Array.from(document.querySelectorAll("li"));
+  let searchText = document.getElementById("searchText").value;
+  let matchCount = 0;
+  for (const word of arrWords) {
+    if (word.textContent.includes(searchText)) {
+      matchCount++;
+      word.style.textDecoration = "underline";
+      word.style.fontWeight = "bold";
+    }
+  }
 }
