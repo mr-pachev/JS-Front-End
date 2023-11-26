8cb2d5b2676dbb4f7f78d4ef5cda4db230558e96
Ivan Pachev <mr.pachev@abv.bg>
2023-11-26 11:56:37 +0200

    Update

--- Exersice/Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ Exersice/Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let rows = Array.from(document.querySelectorAll('tbody td'));
+      let searchField = document.getElementById('searchField').value;
 
+      for (const current of rows) {
+        
+         if(current.textContent.includes(searchField)){
+            console.log('YES');
+         }
+
+      }
    }
 }
