32acee0fb645b77bf81cdd446b65a950fbb7f3e2
Ivan Pachev <mr.pachev@abv.bg>
2023-11-26 11:46:13 +0200

    Update

--- Exersice/Exercise DOM Introduction/04. Search in List/search.js
+++ Exersice/Exercise DOM Introduction/04. Search in List/search.js
@@ -2,11 +2,16 @@ function search() {
   let arrWords = Array.from(document.querySelectorAll("li"));
   let searchText = document.getElementById("searchText").value;
   let matchCount = 0;
+
   for (const word of arrWords) {
+    word.style.textDecoration = "";
+    word.style.fontWeight = "";
     if (word.textContent.includes(searchText)) {
       matchCount++;
       word.style.textDecoration = "underline";
       word.style.fontWeight = "bold";
     }
   }
+
+  document.getElementById("result").textContent = `${matchCount} matches found`;
 }
