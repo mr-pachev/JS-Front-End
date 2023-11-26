86784fd2d2e299d4fabe2eb480195d3a5730f38f
Ivan Pachev <mr.pachev@abv.bg>
2023-11-26 18:51:43 +0200

    Update

--- Exersice/Exercise DOM Introduction/06. Format the Text/solution.js
+++ Exersice/Exercise DOM Introduction/06. Format the Text/solution.js
@@ -6,17 +6,28 @@ function solve() {
   let output = document.getElementById("output"); //полето за изход на данните
 
   let newParagraph = document.createElement("p"); //създаване на параграф
+  let index = 0;
 
   for (const el of inputToArr) {
-    let newEl = el + ".";
-    sentenceArr.push(newEl);
+    index++;
+    let newEl = "";
+
+    if (el.length > 0) {
+      if (index < inputToArr.length) {
+        newEl = el + ".";
+      } else {
+        newEl = el;
+      }
+      sentenceArr.push(newEl);
+    }
   }
 
-  let index = 0;
+  index = 0;
+
   for (const el of sentenceArr) {
     index++;
     if (index === 4) {
-      index = 0;
+      index = 1;
       newParagraph = document.createElement("p");
     }
     newParagraph.textContent += el;
