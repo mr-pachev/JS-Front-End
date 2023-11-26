24bd7524c473cd67a7675794f6f04dd7f9c4bece
Ivan Pachev <mr.pachev@abv.bg>
2023-11-26 22:00:19 +0200

    Update

--- Exersice/Exercise DOM Introduction/07. Hell's Kitchen/app.js
+++ Exersice/Exercise DOM Introduction/07. Hell's Kitchen/app.js
@@ -1,8 +1,17 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector("#btnSend").addEventListener("click", onClick);
 
-   function onClick () {
-      //   TODO:
-      
+  function onClick() {
+    let arr = document.querySelector("#inputs textarea").value;
+    console.log(arr[0]);
+  }
+
+  function getRestInfo(restuaurant){
+   let restuarantData = {};
+
+   for (const iterator of restuaurant) {
+      restuarantData.restName = iterator.split('-');
    }
+   return restuarantData;
+  }
 }
