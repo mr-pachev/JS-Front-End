cebc0355e3781757661f564a4b4f85d5e15ed265
Ivan Pachev <mr.pachev@abv.bg>
2023-11-26 12:07:31 +0200

    Update

--- Exersice/Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ Exersice/Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,16 +1,16 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      let rows = Array.from(document.querySelectorAll('tbody td'));
-      let searchField = document.getElementById('searchField').value;
+  function onClick() {
+    let rows = Array.from(document.querySelectorAll("tbody tr"));
+    let searchField = document.getElementById("searchField").value;
 
-      for (const current of rows) {
-        
-         if(current.textContent.includes(searchField)){
-            console.log('YES');
-         }
+    for (const current of rows) {
+      current.classList.remove("select");
 
+      if (current.textContent.includes(searchField)) {
+        current.classList.add("select");
       }
-   }
+    }
+  }
 }
