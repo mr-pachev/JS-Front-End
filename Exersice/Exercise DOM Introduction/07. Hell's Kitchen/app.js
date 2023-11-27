2d598eba92ad1f862255af331d1db9bce2c12bfe
Ivan Pachev <mr.pachev@abv.bg>
2023-11-27 18:01:16 +0200

    Update

--- Exersice/Exercise DOM Introduction/07. Hell's Kitchen/app.js
+++ Exersice/Exercise DOM Introduction/07. Hell's Kitchen/app.js
@@ -5,91 +5,82 @@ function solve() {
     let arr = document.querySelector("#inputs textarea").value.split(/"(.*?)"/);
 
     arr = arr.filter(function (str) {
-      return str.trim() !== ""; // проверява дали стрингът след използването на trim() не е празен
+      return str.trim() !== "";                             // проверява дали стрингът след използването на trim() не е празен
     });
 
-    for (const iterator of arr) {
-      //премахва новите редове
+    for (const iterator of arr) {                           //премахва новите редове
       arr = arr.map(function (iterator) {
         return iterator.trimStart().trimEnd();
       });
     }
 
-    for (const iterator of arr) {
-      //премахване на елемента ','
+    for (const iterator of arr) {                           //премахване на елемента ','
       if (iterator === ",") {
         let index = arr.indexOf(iterator);
         arr.splice(index, 1);
       }
-    }
+    } 
 
-    pizzaArr = []; //масив с пицариите, като обекти
+    pizzaArr = [];                                          //масив с пицариите, като обекти
 
-    for (const iterator of arr) {
-      //обхожда пицариите от входа
-      let pizzaName = iterator.split(" - ")[0];
-      let currentPizza = new Map();
+    for (const iterator of arr) {                           //обхожда пицариите от входа
+      let pizzaName = iterator.split(" - ")[0];             //взима името на пицарията
+      let currentPizza = new Map();                         //текущата пицария, като map
 
       currentPizza.set("pizzaName", pizzaName);
-      let employeeData = iterator.split(" - ")[1];
+      let employeeData = iterator.split(" - ")[1];          //взима имената на служителите, като string
+
       let countEmployees = 0;
-      let avrSallary = 1;
-      let sumSallary = 0;
-      let bestSallary = 0;
+      let avgSalary = 0;
+      let sumSalary = 0;
+      let bestSalary = 0;
       let workersArr = [];
 
-      for (const worker of employeeData.split(", ")) {
-        //обхожда всяка пицария от входа
+      for (const worker of employeeData.split(", ")) {      //обхожда всяка пицария от входа
         countEmployees++;
 
-        let workerMap = new Map();
+        let workerMap = new Map();                          //map съдържащ информацията за всеки служител: име и заплата
         let workerName = worker.split(" ")[0];
-        let workerSallary = Number(worker.split(" ")[1]);
+        let workerSalary = Number(worker.split(" ")[1]);
 
-        if (bestSallary < workerSallary) {
-          //взима най-голямата заплата
-          bestSallary = workerSallary.toFixed(2);
+        if (bestSalary < workerSalary) {                    //взима най-голямата заплата
+          bestSalary = workerSalary.toFixed(2);
         }
-        sumSallary += workerSallary;
+       
+        sumSalary += workerSalary;
         workerMap.set("workerName", workerName);
-        workerMap.set("workerSallary", workerSallary);
+        workerMap.set("workerSalary", workerSalary);
         workersArr.push(workerMap);
       }
-      avrSallary = sumSallary / countEmployees;
-      avrSallary = avrSallary.toFixed(2); //сумата се показва до втория знак
-      currentPizza.set("avrSallary", avrSallary);
-      currentPizza.set("bestSallary", bestSallary);
+      avgSalary = sumSalary / countEmployees;
+      avgSalary = avgSalary.toFixed(2);                     //сумата се показва до втория знак
+      
+      currentPizza.set("avgSalary", avgSalary);
+      currentPizza.set("bestSalary", bestSalary);
       currentPizza.set("employees", workersArr);
       pizzaArr.push(currentPizza);
     }
 
     let bestPizza = {};
-    let maxAvrSallary = 0;
-    for (const iterator of pizzaArr) {
-      //взима пицарията с най-голяма средна заплата
-      if (maxAvrSallary <= iterator.get("avrSallary")) {
-        maxAvrSallary = iterator.get("avrSallary");
+    let maxAvgSalary = 0;
+
+    for (const iterator of pizzaArr) {                      //взима пицарията с най-голяма средна заплата
+      if (maxAvgSalary <= iterator.get("avgSalary")) {
+        maxAvgSalary = iterator.get("avgSalary");
         bestPizza = iterator;
       }
     }
 
-    document.querySelector(
-      "#bestRestaurant>p"
-    ).textContent = `Name: ${bestPizza.get(
-      "pizzaName"
-    )} Average Salary: ${bestPizza.get(
-      "avrSallary"
-    )} Best Salary: ${bestPizza.get("bestSallary")}`;
+   document.querySelector("#bestRestaurant>p").textContent = `Name: ${bestPizza.get("pizzaName")} Average Salary: ${bestPizza.get("avgSalary")} Best Salary: ${bestPizza.get("bestSalary")}`;
 
-    let emplObj = bestPizza.get("employees");
+   let emplObj = bestPizza.get("employees");                //взима най-добрата пицария, като обект
 
     for (const index in emplObj) {
-      //принтиране втора част
+
       let emplName = emplObj[index].get("workerName");
-      let emplSallary = emplObj[index].get("workerSallary");
-      document.querySelector(
-         "#workers>p"
-       ).textContent += `Name: ${emplName} With Salary: ${emplSallary}`;
+      let emplSalary = emplObj[index].get("workerSalary");
+
+      document.querySelector("#workers p").textContent += `Name: ${emplName} With Salary: ${emplSalary} `;
     }
   }
 }
