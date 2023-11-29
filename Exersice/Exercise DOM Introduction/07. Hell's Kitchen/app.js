29cff035e6ebaa57f2c68093ce5cc8b2a6eb15d5
Ivan Pachev <mr.pachev@abv.bg>
2023-11-29 10:18:11 +0200

    Update

--- Exersice/Exercise DOM Introduction/07. Hell's Kitchen/app.js
+++ Exersice/Exercise DOM Introduction/07. Hell's Kitchen/app.js
@@ -5,23 +5,6 @@ function solve() {
     let input = document.querySelector("#inputs textarea");
     let arr = JSON.parse(input.value);
 
-    arr = arr.filter(function (str) {
-      return str.trim() !== "";                             // проверява дали стрингът след използването на trim() не е празен
-    });
-
-    for (const iterator of arr) {                           //премахва новите редове
-      arr = arr.map(function (iterator) {
-        return iterator.trimStart().trimEnd();
-      });
-    }
-
-    for (const iterator of arr) {                           //премахване на елемента ','
-      if (iterator === ",") {
-        let index = arr.indexOf(iterator);
-        arr.splice(index, 1);
-      }
-    } 
-
     pizzaArr = [];                                          //масив с пицариите, като обекти
 
     for (const iterator of arr) {                           //обхожда пицариите от входа
