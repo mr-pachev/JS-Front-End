34a7e0446cde60e9a6fad8e04f254272a355f793
Ivan Pachev <mr.pachev@abv.bg>
2023-11-30 16:10:47 +0200

    Update

--- Lab/DOM Manipulation and Events/03. Add-Delete/app.js
+++ Lab/DOM Manipulation and Events/03. Add-Delete/app.js
@@ -7,17 +7,17 @@ function addItem() {
     const deleteLink = document.createElement('a');           //създаване на нов елемент
     deleteLink.href = '#';
     deleteLink.textContent = '[Delete]';                      //пълнене на елемента
-    newListItem.append(deleteLink);                           //добавяне на елемента към тага
+    
+    deleteLink.addEventListener('click', deleteRow); 
+    newListItem.appendChild(deleteLink);                      //добавяне на елемента към тага
 
     const list = document.querySelector('#items');            //взимаме елемента, в който трябва да добавим новия елемент
     list.appendChild(newListItem);
     document.querySelector('#newItemText').value = '';        //зачистваме входа
 
-    deleteLink.addEventListener('click', deleteRow); 
 
     function deleteRow(e){
       const rowForDelete = e.currentTarget.parentNode;
-
-      console.log(rowForDelete);
+      rowForDelete.remove();
     }
 }
