5328c295c95d2e66b2fb704402e187db79d41d9e
Ivan Pachev <mr.pachev@abv.bg>
2023-11-30 17:33:14 +0200

    Update

--- Lab/DOM Manipulation and Events/06. Dynamic-Validation/app.js
+++ Lab/DOM Manipulation and Events/06. Dynamic-Validation/app.js
@@ -1,15 +1,17 @@
 function validate() {
-    const inputEmail = document.getElementById('email');
-    inputEmail.addEventListener('change', validate); 
+  const inputEmail = document.getElementById("email");
+  inputEmail.addEventListener("change", validate);
 
-    const pattern = /[a-z]+@[a-z]+\.[a-z]{2,3}/g;
+  const pattern = /[a-z]+@[a-z]+\.[a-z]{2,3}/g;
 
-    function validate(e){
-        let emailAddress = e.currentTarget.value;
+  function validate(e) {
+    let input = e.currentTarget.value;
+    let emailAddress = e.currentTarget;
 
-        if (!pattern.test(emailAddress)){
-            let parent = e.currentTarget; 
-            parent.classList.add('error');
-        }
+    if (!pattern.test(input)) {
+      emailAddress.classList.add("error");
+    } else {
+        emailAddress.classList.remove("error");
     }
+  }
 }
