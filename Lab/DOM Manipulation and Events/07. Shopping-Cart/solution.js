2e5fbef45b4c0184ac01de6be80bb82dbe1b88ee
Ivan Pachev <mr.pachev@abv.bg>
2023-11-30 18:02:59 +0200

    Update

--- Lab/DOM Manipulation and Events/07. Shopping-Cart/solution.js
+++ Lab/DOM Manipulation and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-   //TODO...
+  //взима масив от всички натиснати бутони
+  const productSelected = Array.from(
+    document.querySelectorAll("button.add-product")
+  );
+
+  for (const product of productSelected) {
+    product.addEventListener("click", addProduct); //събитие за всеки натиснат продукт
+  }
+
+  let selectedProducts = [];
+  let totalPrice = 0;
+
+  function addProduct(e) {
+      let productData = e.currentTarget.parentNode.parentNode; ;
+      let productName = productData.querySelector('.product-title').textContent;
+      let productPrice = productData.querySelector('.product-line-price').textContent;
+
+      totalPrice += Number(productPrice);
+      selectedProducts.push(productName);
+
+      console.log(productPrice);
+  }
+
 }
