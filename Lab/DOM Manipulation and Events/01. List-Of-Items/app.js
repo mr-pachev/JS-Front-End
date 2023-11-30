cb9ce55db1a9244a979fc562b0a3b8d60ee94258
Ivan Pachev <mr.pachev@abv.bg>
2023-11-30 15:02:03 +0200

    Update

--- Lab/DOM Manipulation and Events/01. List-Of-Items/app.js
+++ Lab/DOM Manipulation and Events/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    const text = document.querySelector('#newItemText').value;
+
+    const newListItem = document.createElement('li');   //създаване на нов елемент
+    newListItem.textContent = text;                     //пълнене на елемента
+
+    const list = document.querySelector('#items');
+    list.appendChild(newListItem);
+    document.querySelector('#newItemText').value = '';
 }
