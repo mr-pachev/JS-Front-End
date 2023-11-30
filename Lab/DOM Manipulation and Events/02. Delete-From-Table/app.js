794fafefcfe0b4e02be362de5cd6f8fd2d4f1596
Ivan Pachev <mr.pachev@abv.bg>
2023-11-30 15:21:08 +0200

    Update

--- Lab/DOM Manipulation and Events/02. Delete-From-Table/app.js
+++ Lab/DOM Manipulation and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,12 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const inputEmail = document.querySelector('input[name="email"]');
+
+  const listEmail = Array.from(document.querySelector('#customers tbody').children);
+
+  for (const row of listEmail) {
+    const selectedValue = row.children[1];	//взима стойността на реда
+
+    
+}
+
 }
