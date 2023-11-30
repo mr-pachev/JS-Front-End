17a9e64d0b59fb295effec8e408b152eeeae4085
Ivan Pachev <mr.pachev@abv.bg>
2023-11-30 16:29:16 +0200

    Update

--- Lab/DOM Manipulation and Events/04. Mouse-Gradient/app.js
+++ Lab/DOM Manipulation and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.querySelector('#gradient');
+
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+    
+    function gradientMove(e){
+        let power = e.offsetX / (e.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+
+        document.querySelector('#result').textContent = power + '%';
+    }
+
+    function gradientOut(){
+        document.querySelector('#result').textContent = '';
+    }
 }
