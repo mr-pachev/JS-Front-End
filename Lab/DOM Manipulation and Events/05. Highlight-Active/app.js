f1e3f26af684b55a7ed3653110a9bfa36dbbf85d
Ivan Pachev <mr.pachev@abv.bg>
2023-11-30 16:57:29 +0200

    Update

--- Lab/DOM Manipulation and Events/05. Highlight-Active/app.js
+++ Lab/DOM Manipulation and Events/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+  const input = Array.from(document.getElementsByTagName("input"));
+
+  for (const div of input) {
+    div.addEventListener("focus", functionFocus);
+    div.addEventListener("blur", functionBlur);
+  }
+
+  function functionFocus(e) {
+    let div = e.currentTarget.parentNode;
+
+    div.classList.add("focused");
+  }
+
+  function functionBlur(e) {
+    let div = e.currentTarget.parentNode;
+
+    div.classList.remove("focused");
+  }
 }
