c62ea45c7cef909c2943a0bdc027fafe01a6ef67
Ivan Pachev <mr.pachev@abv.bg>
2023-12-03 19:26:29 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/01. Sections/script.js
+++ Exersice/Exercise DOM Manipulation and Events/01. Sections/script.js
@@ -1,12 +1,21 @@
 function create(words) {
-   let divContent = document.getElementById('content');
+  let divContent = document.getElementById("content"); //взимаме DOM елемента, в който ще вкарбаме съдържанието
 
-   for (const word of words) {
-      let div = document.createElement('div');
-      let p = document.createElement('p');
+  for (const word of words) { //обхожда всички думи от входа
+    let div = document.createElement("div"); 
+    let p = document.createElement("p");
 
-      p.textContent = word;
+    p.textContent = word; //пълним параграфа
+    p.style.display = 'none';
 
-      console.log(p);
-   }
+    div.appendChild(p); //добавяме параграфа към div
+
+    divContent.appendChild(div); //закачаме тага към DOM елемента
+
+    div.addEventListener("click", onAddContent);
+
+    function onAddContent() {
+      p.style.display = 'block';
+    }
+  }
 }
