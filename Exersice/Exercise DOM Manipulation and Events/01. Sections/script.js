a32c2ffd2a35995052cd153f63d12fb81577ecd3
Ivan Pachev <mr.pachev@abv.bg>
2023-12-03 18:12:07 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/01. Sections/script.js
+++ Exersice/Exercise DOM Manipulation and Events/01. Sections/script.js
@@ -2,6 +2,11 @@ function create(words) {
    let divContent = document.getElementById('content');
 
    for (const word of words) {
-      let div = cre
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+
+      p.textContent = word;
+
+      console.log(p);
    }
 }
