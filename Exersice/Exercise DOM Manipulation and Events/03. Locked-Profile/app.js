aee871caee87ec6147b95dbe06dc842f67e40fd9
Ivan Pachev <mr.pachev@abv.bg>
2023-12-03 20:45:24 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/03. Locked-Profile/app.js
+++ Exersice/Exercise DOM Manipulation and Events/03. Locked-Profile/app.js
@@ -1,3 +1,16 @@
 function lockedProfile() {
-    console.log('TODO...')
+  let buttons = Array.from(document.getElementsByTagName("button"));
+
+  for (const button of buttons) {
+    buttonCondition.addEventListener("click", checked);
+
+    function checked(e) {
+      let profile = button.parentNode;
+
+      let profileChildren = Array.from(profile.children);
+      let buttonCondition = profileChildren[10];
+      let radioBtn = profileChildren[2];
+      let hiddenFiels = profileChildren[9];
+    }
+  }
 }
