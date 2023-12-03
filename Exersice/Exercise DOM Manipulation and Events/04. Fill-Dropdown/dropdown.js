fa7305f4a335b38d8b1f3e896b60aa184a7c208a
Ivan Pachev <mr.pachev@abv.bg>
2023-12-03 21:42:37 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/04. Fill-Dropdown/dropdown.js
+++ Exersice/Exercise DOM Manipulation and Events/04. Fill-Dropdown/dropdown.js
@@ -1,10 +1,14 @@
 function addItem() {
-  const parentEl = document.querySelector("article").children;
+  const parentEl = Array.from(document.querySelector("article").children);
 
   const text = parentEl[2];
   const value = parentEl[4];
 
-  
+  let op = document.createElement("option");
 
-  console.log(parentEl);
+  op.textContent = text.value;
+  op.value = value.value;
+
+  document.getElementById("menu").appendChild(op);
+  parentEl[6].disabled = false; 
 }
