852ab279c5ab195d91f18dafa48f5473224f03b1
Ivan Pachev <mr.pachev@abv.bg>
2023-12-03 19:43:20 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/02. Time-Converter/solution.js
+++ Exersice/Exercise DOM Manipulation and Events/02. Time-Converter/solution.js
@@ -1,4 +1,19 @@
 function attachEventsListeners() {
+  let days = document.getElementById("days");
+  let daysBtn = document.getElementById("daysBtn");
 
-    console.log('TODO:');
+  let hours = document.getElementById("hours");
+  let hoursBtn = document.getElementById("hoursBtn");
+
+  let minutes = document.getElementById("minutes");
+  let minutesBtn = document.getElementById("minutesBtn");
+
+  let seconds = document.getElementById("seconds");
+  let secondsBtn = document.getElementById("secondsBtn");
+
+  daysBtn.addEventListener("click", () => {
+    hours.value = Number(days.value) * 24;
+    minutes.value = Number(hours.value) * 60;
+    seconds.value = Number(minutes.value) * 60;
+  });
 }
