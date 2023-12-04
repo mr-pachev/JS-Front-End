52e6f314782b47858630365c284f3548641fc7c9
Ivan Pachev <mr.pachev@abv.bg>
2023-12-04 12:46:22 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
+++ Exersice/Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
@@ -2,11 +2,19 @@ function encodeAndDecodeMessages() {
     let textFields = Array.from(document.getElementsByTagName('textarea'));
     let buttons = Array.from(document.getElementsByTagName('button'));
 
-    let encodecodedText = textFields[0];
-    let decodedText = textFields[1];
+    let encodeText = textFields[0];     //некодиран текст
+    let decodeText = textFields[1];     //кодиран текст
 
     let btnEncode = buttons[0];
-    let btnDencode = buttons[1];
+    let btnDecode = buttons[1];
 
-    
+    btnEncode.addEventListener('click', encoded);
+
+    function encoded(){                 //кодиране на текста
+        let inputText = encodeText.value.split('');
+
+        for (const char of inputText) {
+            console.log(char.charCodeAt(0));
+        }
+    }
 }
