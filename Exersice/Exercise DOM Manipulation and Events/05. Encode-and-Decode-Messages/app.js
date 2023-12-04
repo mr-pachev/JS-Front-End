4855dc8a4f016e15248a701483923e8469d3a804
Ivan Pachev <mr.pachev@abv.bg>
2023-12-04 12:58:12 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
+++ Exersice/Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,26 +1,44 @@
 function encodeAndDecodeMessages() {
-    let textFields = Array.from(document.getElementsByTagName('textarea'));
-    let buttons = Array.from(document.getElementsByTagName('button'));
+  let textFields = Array.from(document.getElementsByTagName("textarea"));
+  let buttons = Array.from(document.getElementsByTagName("button"));
 
-    let encodeText = textFields[0];     //некодиран текст
-    let decodeText = textFields[1];     //кодиран текст
+  let encodeText = textFields[0]; //некодиран текст
+  let decodeText = textFields[1]; //кодиран текст
 
-    let btnEncode = buttons[0];
-    let btnDecode = buttons[1];
+  let btnEncode = buttons[0];
+  let btnDecode = buttons[1];
 
-    btnEncode.addEventListener('click', encoded);
+  btnEncode.addEventListener("click", encoded);
 
-    function encoded(){                 //кодиране на текста
-        let inputText = encodeText.value.split('');
-        let result = '';
+  function encoded() {
+    //кодиране на текста
+    let inputText = encodeText.value.split("");
+    let result = "";
 
-        for (const char of inputText) {
-           let ASCiiCode = char.charCodeAt(0) + 1;
+    for (const char of inputText) {
+      let ASCiiCode = char.charCodeAt(0) + 1;
 
-           result += String.fromCharCode(ASCiiCode);
-        }
-        
-        decodeText.value = result;
-        encodeText.value = '';
+      result += String.fromCharCode(ASCiiCode);
     }
+
+    decodeText.value = result;
+    encodeText.value = "";
+  }
+
+  btnDecode.addEventListener("click", decoded);
+
+  function decoded() {
+    //кодиране на текста
+    let inputText = decodeText.value.split("");
+    let result = "";
+
+    for (const char of inputText) {
+      let ASCiiCode = char.charCodeAt(0) - 1;
+
+      result += String.fromCharCode(ASCiiCode);
+    }
+
+    encodeText.value = result;
+    decodeText.value = "";
+  }
 }
