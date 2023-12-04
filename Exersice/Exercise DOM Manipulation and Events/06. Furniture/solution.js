a0c7b6314c55de49b4ad30870d0169a169d2443c
Ivan Pachev <mr.pachev@abv.bg>
2023-12-04 18:48:02 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/06. Furniture/solution.js
+++ Exersice/Exercise DOM Manipulation and Events/06. Furniture/solution.js
@@ -1,4 +1,26 @@
 function solve() {
+  let generateBtn = document.querySelector("#exercise button");
 
-  //TODO...
+  generateBtn.addEventListener("click", createObj);
+
+  function createObj(e) {
+    let textarea = document.querySelector("textarea").value;
+    let furnitureInfo = JSON.parse(textarea);   //обект от входа
+    
+
+    for (const iterator of furnitureInfo) {     //итерация по стойностите на обекта
+      let trFurniture = document.createElement('tr');  
+      let tbodyTag = document.querySelector('tbody');
+      tbodyTag.appendChild(trFurniture);
+      
+
+
+      let img = iterator.img;
+      let name = iterator.name;
+      let price = iterator.price;
+      let decFactor = iterator.decFactor;
+
+      console.log(img, name, price, decFactor);
+    }
+  }
 }
