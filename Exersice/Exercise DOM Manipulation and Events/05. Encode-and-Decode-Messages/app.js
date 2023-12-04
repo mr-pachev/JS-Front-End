fb92f2aac8c6aba82005e4f5040ed31fe197af86
Ivan Pachev <mr.pachev@abv.bg>
2023-12-04 12:29:56 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
+++ Exersice/Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,12 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let textFields = Array.from(document.getElementsByTagName('textarea'));
+    let buttons = Array.from(document.getElementsByTagName('button'));
+
+    let encodecodedText = textFields[0];
+    let decodedText = textFields[1];
+
+    let btnEncode = buttons[0];
+    let btnDencode = buttons[1];
+
+    
 }
