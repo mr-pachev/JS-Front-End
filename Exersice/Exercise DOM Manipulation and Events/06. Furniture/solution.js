ff7baf4f73ca33f6aa514ac32459d89db52782a3
Ivan Pachev <mr.pachev@abv.bg>
2023-12-06 14:14:16 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/06. Furniture/solution.js
+++ Exersice/Exercise DOM Manipulation and Events/06. Furniture/solution.js
@@ -5,33 +5,31 @@ function solve() {
 
   function createObj(e) {
     let textarea = document.querySelector("textarea").value;
-    let furnitureInfo = JSON.parse(textarea);   //обект от входа
-    
+    let furnitureInfo = JSON.parse(textarea); //обект от входа
 
-    for (const iterator of furnitureInfo) {     //итерация по стойностите на обекта
+    for (const iterator of furnitureInfo) {
+      //итерация по стойностите на обекта
       let img = iterator.img;
       let name = iterator.name;
       let price = iterator.price;
       let decFactor = iterator.decFactor;
-      
-      console.log(img);
-      
-      let tbodyTag = document.querySelector('tbody');
-      let trFurniture = document.createElement('tr'); 
-      
-      let tdImg = document.createElement('td');
-      let pImg = document.createElement('p');
-      let image = document.createElement('img');
+      let tbodyTag = document.querySelector("tbody");
 
-      image.setAttribute('src', img);
+      let trFurniture = document.createElement("tr");
+
+      tbodyTag.appendChild(creatTdImg(img, trFurniture));
+    }
+
+    function creatTdImg(img, trFurniture) {
+      let tdImg = document.createElement("td");
+      let pImg = document.createElement("p");
+
+      let image = document.createElement("img");
+      image.setAttribute("src", img);
 
       pImg.appendChild(image);
-     
       tdImg.appendChild(pImg);
-      trFurniture.appendChild(tdImg);
-
-      tbodyTag.appendChild(trFurniture);
-      
+      return trFurniture.appendChild(tdImg);
     }
   }
 }
