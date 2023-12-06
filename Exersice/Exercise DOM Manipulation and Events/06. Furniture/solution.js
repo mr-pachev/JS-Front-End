0f765f98d1e28b4aef85006511861470bf7d2fb2
Ivan Pachev <mr.pachev@abv.bg>
2023-12-06 15:11:29 +0200

    Update

--- Exersice/Exercise DOM Manipulation and Events/06. Furniture/solution.js
+++ Exersice/Exercise DOM Manipulation and Events/06. Furniture/solution.js
@@ -1,48 +1,45 @@
 function solve() {
   let generateBtn = document.querySelector("#exercise button");
+  let tbodyTag = document.querySelector("tbody");
 
   generateBtn.addEventListener("click", createObj);
 
   function createObj(e) {
     let textarea = document.querySelector("textarea").value;
     let furnitureInfo = JSON.parse(textarea); //обект от входа
-
-    for (const iterator of furnitureInfo) {
-      //итерация по стойностите на обекта
+  
+    for (const iterator of furnitureInfo) {   //итерация по стойностите на обекта
       let img = iterator.img;
       let name = iterator.name;
       let price = iterator.price;
       let decFactor = iterator.decFactor;
-      let tbodyTag = document.querySelector("tbody");
-
       let trFurniture = document.createElement("tr");
-
+      
       trFurniture.appendChild(creatTdImg(img));
       trFurniture.appendChild(creatTdName(name));
 
-      tbodyTag.appendChild(creatTdImg(trFurniture));
+      tbodyTag.appendChild(trFurniture);
     }
+  }
 
-    function creatTdImg(img) { //създаване и добавяне на колона с картина в реда
-      let tdImg = document.createElement("td");
-      let pImg = document.createElement("p");
+  function creatTdImg(img) {      //създаване и добавяне на колона с картина в реда
+    let tdImg = document.createElement("td");
+    let pImg = document.createElement("p");
 
-      let image = document.createElement("img");
-      image.setAttribute("src", img);
+    let image = document.createElement("img");
+    image.setAttribute("src", img);
 
-      pImg.appendChild(image);
-      return tdImg.appendChild(pImg);
-    }
+    pImg.appendChild(image);
+    return tdImg.appendChild(pImg);
+  }
 
-    function creatTdName(name) { //създаване и добавяне на колона с името в реда
-      let tdName = document.createElement("td");
-      let pName = document.createElement("p");
+  function creatTdName(name) {    //създаване и добавяне на колона с името в реда
+    let tdName = document.createElement("td");
+    let pName = document.createElement("p");
 
-      pName.textContent = name;
-      console.log(pName);
-      return tdName.appendChild(pName);
-    }
+    pName.setAttribute('class', name)
 
-
+    pName.textContent = name;
+    return tdName.appendChild(pName);
   }
 }
