e45389b210cbb78f912c8c83e955471d8a5d145e
Ivan Pachev <mr.pachev@abv.bg>
2023-12-07 21:35:31 +0200

    Update

--- Lab/HTTP and REST/05.Book-library/app.js
+++ Lab/HTTP and REST/05.Book-library/app.js
@@ -1,9 +1,12 @@
 function attachEvents() {
     const loadBooks = document.getElementById('loadBooks');
     const booksContainer = document.querySelector('table > tbody');
+    const [titleInput, authorInput] = Array.from(document.querySelectorAll('#form > input'));
+    const submitBtn = document.querySelector('#form > button');
     const BASE_URL = 'http://localhost:3030/jsonstore/collections/books/';
 
     loadBooks.addEventListener('click', loadAllBooksHandler);
+    submitBtn.addEventListener('click', createBookHandler);
 
     async function loadAllBooksHandler(){
         booksContainer.innerHTML = '';
@@ -18,7 +21,23 @@ function attachEvents() {
             const buttonsColumn = document.createElement('td');
             const editBtn = document.createElement('button');
             const deleteBtn = document.createElement('button');
+            titleColumn.textContent = title;
+            authorColumn.textContent = autor;
+            editBtn.textContent = 'Edit';
+            deleteBtn.textContent = 'Delete';
+
+            //DOM Manipulations
+            tableRow.appendChild(titleColumn);
+            tableRow.appendChild(authorColumn);
+            buttonsColumn.appendChild(editBtn);
+            buttonsColumn.appendChild(deleteBtn);
+            tableRow.appendChild(buttonsColumn);
+            booksContainer.appendChild(tableRow);
         }
+    }
+
+    async function createBookHandler(event){
+        
     }
 }
 
