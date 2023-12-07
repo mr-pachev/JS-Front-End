8cab11918f682bfe51e3510caadbc4ddcfb125b6
Ivan Pachev <mr.pachev@abv.bg>
2023-12-07 21:08:06 +0200

    Update

--- Lab/Asynchronous Programming/01.Bus-Stop/app.js
+++ Lab/Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,19 +1,24 @@
 function getInfo() {
-    const stopIdInput = document.querySelector('#stopId'); //номера на спирката от входа
-    const BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/:';
-    const stopNameContainer = document.getElementById('stopName'); //мястото за изписване името на сприката
-    const busesContainer = document.getElementById('buses'); //мястото да изписване на инфото за автобусите
-    const stopId = stopIdInput.value; //номера на спирката
+  const stopIdInput = document.querySelector("#stopId"); //номера на спирката от входа
+  const BASE_URL = "http://localhost:3030/jsonstore/bus/businfo/";
+  const stopNameContainer = document.getElementById("stopName"); //мястото за изписване името на сприката
+  const busesContainer = document.getElementById("buses"); //мястото да изписване на инфото за автобусите
+  const stopId = stopIdInput.value; //номера на спирката
 
+  fetch(`${BASE_URL}${stopId}`)
+    .then((res) => res.json())
+    .then((result) => {
+      const { name, buses } = result;   
+      stopNameContainer.textContent = name;
 
-    fetch(`${BASE_URL}${stopId}`)
-        .then((res) => res.json())
-        .then((result) => {
-           const {name, buses} = result;
-           stopNameContainer.textContent = name;
-
-        })
-        .catch((err) => {
-
-        });
+      busesContainer.textContent = '';
+      for (const busId in buses) {
+        let li = document.createElement('li');
+        li.textContent = `Bus ${busId} arrives in ${buses[busId]} minutes`;
+        busesContainer.appendChild(li);
+      }
+    })
+    .catch((err) => {
+        stopNameContainer.textContent = 'Error';
+    });
 }
