7f2de43b757760e848170a0f501ba149e3dd2bc0
Ivan Pachev <mr.pachev@abv.bg>
2023-12-07 17:51:51 +0200

    Update

--- Lab/Asynchronous Programming/01.Bus-Stop/app.js
+++ Lab/Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,10 @@
 function getInfo() {
-    // TODO:
+    const stopId = document.querySelector('#stopId'); //номера на спирката от входа
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/:';
+
+    fetch(`${stopId}${BASE_URL}`)
+        .then(res => res.json)
+        .then(result => {
+            
+        })
 }
