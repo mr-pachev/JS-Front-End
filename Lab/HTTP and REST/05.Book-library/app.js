e545688bc8d0a61afd3db790cae399323645e16f
Ivan Pachev <mr.pachev@abv.bg>
2023-12-07 21:16:15 +0200

    Update

--- Lab/HTTP and REST/05.Book-library/app.js
+++ Lab/HTTP and REST/05.Book-library/app.js
@@ -5,7 +5,21 @@ function attachEvents() {
 
     loadBooks.addEventListener('click', loadAllBooksHandler);
 
-    
+    async function loadAllBooksHandler(){
+        booksContainer.innerHTML = '';
+        const booksRes = await fetch(BASE_URL);
+        const booksData = await booksRes.json();
+
+        for (const bookId in booksData){
+            const {autor, title} = booksData[bookId];
+            const tableRow = document.createElement('tr');
+            const titleColumn = document.createElement('td');
+            const authorColumn = document.createElement('td');
+            const buttonsColumn = document.createElement('td');
+            const editBtn = document.createElement('button');
+            const deleteBtn = document.createElement('button');
+        }
+    }
 }
 
 attachEvents();
