efa982aacf128b314df3aad96d5cad7c9ad7714f
Ivan Pachev <mr.pachev@abv.bg>
2023-12-08 21:38:48 +0200

    Update

--- Lab/Asynchronous Programming/02.Bus-Schedule/app.js
+++ Lab/Asynchronous Programming/02.Bus-Schedule/app.js
@@ -3,25 +3,43 @@ function solve() {
     const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
     const departBtn = document.getElementById('depart');
     const arriveBtn = document.getElementById('arrive');
-    let busStopId = 'depot';
+    let nextBusStopId = 'depot';
+    let busStopName = '';
 
     function depart() {
-        document.getElementById('depart').disabled = true; 
-        document.getElementById('arrive').disabled = false; 
+        departBtn.disabled = true; 
+        arriveBtn.disabled = false; 
 
-        fetch(`${BASE_URL}${busStopId}`)
+        fetch(`${BASE_URL}${nextBusStopId}`)
         .then((res) => res.json())
         .then((busStopInfo) => {
             const {name, next} = busStopInfo;
+            busStopName = name;
            
             contentBox.textContent = `Next stop ${name}`;
-            busStopId = next;
+            nextBusStopId = next;
         })
+        .catch(() => {
+            departBtn.disabled = true; 
+            arriveBtn.disabled = true; 
+        })
+        
     }
 
     async function arrive() {
-        departBtn.disabled = true; 
-        arriveBtn.disabled = false; 
+        departBtn.disabled = false; 
+        arriveBtn.disabled = true; 
+
+        fetch(`${BASE_URL}${nextBusStopId}`)
+        .then((res) => res.json())
+        .then((busStopInfo) => {
+            const {name, next} = busStopInfo;
+
+            console.log(next);
+           
+            contentBox.textContent = `Arriving at ${busStopName}`;
+            nextBusStopId = next;
+        })
     }
 
     return {
