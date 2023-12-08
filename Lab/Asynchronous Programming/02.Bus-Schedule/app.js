6f376a7de93d79bbf294ea47f11cd76ed2ee1d1b
Ivan Pachev <mr.pachev@abv.bg>
2023-12-08 21:17:19 +0200

    Update

--- Lab/Asynchronous Programming/02.Bus-Schedule/app.js
+++ Lab/Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,10 +1,27 @@
 function solve() {
+    const contentBox = document.getElementById('info');
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const busStopId = 'depot';
+
     function depart() {
-        // TODO: 
+        departBtn.disabled = false; 
+        arriveBtn.disabled = true; 
+
+        fetch(`${BASE_URL}${busStopId}`)
+        .then((res) => res.json())
+        .then((busStopInfo) => {
+            const {name, next} = busStopInfo;
+           
+            contentBox.textContent = `Next stop ${stopName}`;
+            busStopId = next;
+        })
     }
 
     async function arrive() {
-        // TODO:
+        departBtn.disabled = true; 
+        arriveBtn.disabled = false; 
     }
 
     return {
