59d535e6a1399cb790d7c116086457050e5fca17
Ivan Pachev <mr.pachev@abv.bg>
2023-12-09 16:54:42 +0200

    Update

--- Lab/HTTP and REST/03. Github-Repos-by-Username/scripts/app.js
+++ Lab/HTTP and REST/03. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,7 @@
+const BASE_URL = 'https://api.github.com/users/';
+const username = document.getElementById('username');
+const ul = document.getElementById('repos');
+
 function loadRepos() {
 	console.log("TODO...");
 }
