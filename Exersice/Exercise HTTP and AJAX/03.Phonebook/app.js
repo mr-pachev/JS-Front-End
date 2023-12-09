8e97d9171e66e99cc1d5999b0fd059108e902940
Ivan Pachev <mr.pachev@abv.bg>
2023-12-09 20:19:07 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/03.Phonebook/app.js
+++ Exersice/Exercise HTTP and AJAX/03.Phonebook/app.js
@@ -1,5 +1,27 @@
 function attachEvents() {
-    // TODO:
+  const loadBtn = document.getElementById("btnLoad");
+  const ul = document.getElementById("phonebook");
+  const BASE_URL = "http://localhost:3030/jsonstore/phonebook/";
+
+  loadBtn.addEventListener("click", loadPhoneBook);
+
+  async function loadPhoneBook() {
+    try {
+      const phoneBook = await fetch(BASE_URL);
+      let phoneBookData = await phoneBook.json();
+      const li = document.createElement('li');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Delete';
+
+      const values = Object.values(phoneBookData);
+
+      for (const { person, phone, _id } of values) {
+        console.log(person);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 attachEvents();
