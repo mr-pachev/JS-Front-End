03cd106f2fe5178ea8192ef2da536e107071bbe9
Ivan Pachev <mr.pachev@abv.bg>
2023-12-09 20:56:08 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/03.Phonebook/app.js
+++ Exersice/Exercise HTTP and AJAX/03.Phonebook/app.js
@@ -1,26 +1,55 @@
 function attachEvents() {
   const loadBtn = document.getElementById("btnLoad");
+  const createBtn = document.getElementById("btnCreate");
   const ul = document.getElementById("phonebook");
+  const personInput = document.getElementById("person");
+  const phoneInput = document.getElementById("phone");
   const BASE_URL = "http://localhost:3030/jsonstore/phonebook/";
 
   loadBtn.addEventListener("click", loadPhoneBook);
+  createBtn.addEventListener("click", createPhoneAddress);
 
   async function loadPhoneBook() {
     try {
       const phoneBook = await fetch(BASE_URL);
       let phoneBookData = await phoneBook.json();
-      const li = document.createElement('li');
-      const deleteBtn = document.createElement('button');
-      deleteBtn.textContent = 'Delete';
 
       const values = Object.values(phoneBookData);
 
+      ul.textContent = "";
       for (const { person, phone, _id } of values) {
-        console.log(person);
+        const li = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.id = this.id;
+
+        li.textContent = `${person}: ${phone}`;
+        li.appendChild(deleteBtn);
+        ul.appendChild(li);
       }
     } catch (err) {
       console.error(err);
     }
+  }
+
+   function createPhoneAddress() {
+    let person = personInput.value;
+    let phone = phoneInput.value;
+    
+    fetch(BASE_URL, {
+        method: 'post',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({person, phone}),
+        })
+        .then(() => {
+            person = '';
+            phone = '';
+            loadPhoneBook();
+        })
+        .catch((err) => console.error(err));
+
+
+    
   }
 }
 
