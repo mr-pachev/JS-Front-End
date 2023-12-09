1ac3be7c539e7bf9ea64762f6c7e518ee89c3151
Ivan Pachev <mr.pachev@abv.bg>
2023-12-09 13:00:23 +0200

    Update

--- Lab/Asynchronous Programming/03.Forecaster/app.js
+++ Lab/Asynchronous Programming/03.Forecaster/app.js
@@ -1,21 +1,20 @@
-const inputLocation = document.querySelector('#request > #location');
-const buttonGetWeather = document.querySelector('#request > #submit');
-let BASE_URL = 'http://localhost:3030/jsonstore/forecaster/locations';
-
-buttonGetWeather.addEventListener('click', attachEvents)
-
 function attachEvents() {
+  const inputLocation = document.querySelector("#request > #location");
+  const buttonGetWeather = document.querySelector("#request > #submit");
+  let BASE_URL = "http://localhost:3030/jsonstore/forecaster/locations";
+
+  buttonGetWeather.addEventListener("click", getLocation);
+
+  function getLocation() {
     fetch(BASE_URL)
-    .then((res) => res.json())
-    .then((locationInfo) => {
+      .then((res) => res.json())
+      .then((locationInfo) => {
         for (const key in locationInfo) {
-           console.log(key);
+          console.log(key);
         }
-    })
-    .catch((err) => {
-
-    })
+      })
+      .catch((err) => {});
+  }
 }
-
 
 attachEvents();
