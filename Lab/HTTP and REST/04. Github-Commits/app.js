36d9f1454cca2ff17ccaabafd8f6f8fa83dbf306
Ivan Pachev <mr.pachev@abv.bg>
2023-12-09 16:45:39 +0200

    Update

--- Lab/HTTP and REST/04. Github-Commits/app.js
+++ Lab/HTTP and REST/04. Github-Commits/app.js
@@ -1,15 +1,20 @@
 const BASE_URL = "https://api.github.com/repos";
 const username = document.getElementById("username").value;
 const idRepo = document.getElementById("repo").value;
+const ul = document.getElementById('commits');
 
 function loadCommits() {
   fetch(`${BASE_URL}/${username}/${idRepo}/commits`)
     .then((res) => res.json())
     .then((data) => {
-      data.foreach(({ comit }) => {
-        console.log('comit.author.name');
-      });
+      
+       data
+            .forEach(({commit}) => {
+                let li = document.createElement('li');
+                li.textContent = `${commit.author.name}: ${commit.message}` ;
+                ul.appendChild(li);
+            });
     })
-    .catch((err) => console.error(`Error: <error.status> (Not Found)`));
+    .catch((error) => console.error(`Error: ${error.status} (Not Found)`));
 
 }
