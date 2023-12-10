682d998c5d090b6e8f66fd59e3132c39e83e1dfe
Ivan Pachev <mr.pachev@abv.bg>
2023-12-10 11:24:17 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/01.Blog/app.js
+++ Exersice/Exercise HTTP and AJAX/01.Blog/app.js
@@ -1,5 +1,18 @@
 function attachEvents() {
-    // TODO:
+    const loadPostBtn = document.getElementById('btnLoadPosts');
+    const viewPostBtn = document.getElementById('btnViewPost');
+    let selectContainer= document.getElementById('post');
+    const BAS_URL = 'http://localhost:3030/jsonstore/blog/';
+     
+     loadPostBtn.addEventListener('click', async() => {
+        const posts = await fetch(BAS_URL + 'posts');
+        const postsContent = await posts.json();
+
+        console.log(postsContent);
+     });
+
+     
+
 }
 
 attachEvents();
