4e6634746c2b16991a8177d2aa64d99bba7c8490
Ivan Pachev <mr.pachev@abv.bg>
2023-12-10 18:21:18 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/02.Messenger/app.js
+++ Exersice/Exercise HTTP and AJAX/02.Messenger/app.js
@@ -1,5 +1,17 @@
 function attachEvents() {
-    // TODO:
+    const textContainer = document.getElementById('messages');
+    const nameInput = document.querySelector('input[name="author"]');
+    const messageInput = document.querySelector('input[name="content"]');
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const BASE_URL = 'http://localhost:3030/jsonstore/messenger/';
+
+    sendBtn.addEventListener('click', async() => {
+        // const personMessage = {'author': nameInput.value, 'content': messageInput.value};
+
+        console.log(nameInput.value);
+        console.log(messageInput.value);
+    })
 }
 
 attachEvents();
