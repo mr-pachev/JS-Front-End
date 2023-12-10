e6364f08eecc283524cd9b01638e668867f03acf
Ivan Pachev <mr.pachev@abv.bg>
2023-12-10 19:41:55 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/02.Messenger/app.js
+++ Exersice/Exercise HTTP and AJAX/02.Messenger/app.js
@@ -7,12 +7,13 @@ function attachEvents() {
   const BASE_URL = "http://localhost:3030/jsonstore/messenger/";
 
   sendBtn.addEventListener("click", async () => {
-    //създаваме обект с име и текст
+    //създаваме обект с име и текст от входа
     const personMessage = {
       author: nameInput.value,
       content: messageInput.value,
     };
 
+    //изпращаме заявка за добавяне на новия обект
     fetch(BASE_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -23,6 +24,23 @@ function attachEvents() {
       })
       .then(() => {})
       .catch((err) => console.log(err));
+  });
+
+  refreshBtn.addEventListener("click", async () => {
+    textContainer.innerHTML = '';
+    try {
+      const messages = await fetch(BASE_URL);
+      let messageContent = await messages.json();
+
+      const values = Object.values(messageContent);
+      
+      for (const obj of values) {
+        textContainer.innerHTML += `${obj.author}: ${obj.content}\n`
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
   });
 }
 
