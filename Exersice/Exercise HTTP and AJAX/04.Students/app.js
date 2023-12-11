66f6c1d684f609712ba97ee7bd0d5d4f1b690f09
Ivan Pachev <mr.pachev@abv.bg>
2023-12-11 12:03:33 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/04.Students/app.js
+++ Exersice/Exercise HTTP and AJAX/04.Students/app.js
@@ -1,17 +1,23 @@
 function attachEvents() {
-  const inputFirstName = document.querySelector('input[name=”firstName”]'); 
-  const inputlLastName = document.querySelector('input[name=”lastName”]'); 
-  const inputFacultyNumber = document.querySelector('input[name=”facultyNumber”]'); 
-  const inputGrade = document.querySelector('input[name=”grade”]'); 
-  const submitBTN = document.getElementById('submit');
+  const inputFirstName = document.querySelector('input[name="firstName"]'); 
+  const inputlLastName = document.querySelector('input[name="lastName"]'); 
+  const inputFacultyNumber = document.querySelector('input[name="facultyNumber"]'); 
+  const inputGrade = document.querySelector('input[name="grade"]'); 
+  const submitBtn = document.getElementById('submit');
 
+  submitBtn.addEventListener('click', async() => {
+    let studentObj = {
+        "firstName": inputFirstName.value,
+        "lastName": inputlLastName.value,
+        "facultyNumber": inputFacultyNumber,
+        "grade": Number(inputGrade.value).toFixed(2),
+    }
+    
+    console.log(studentObj)
+  })
 
 
   
-  console.log(inputFirstName.value);
-  console.log(inputlLastName.value);
-  console.log(inputFacultyNumber.value);
-  console.log(inputGrade.value);
 }
 
 attachEvents();
