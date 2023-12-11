cc2e6d77752ea31ff17b7ff668e66af79c36aebe
Ivan Pachev <mr.pachev@abv.bg>
2023-12-11 11:49:54 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/04.Students/app.js
+++ Exersice/Exercise HTTP and AJAX/04.Students/app.js
@@ -1,5 +1,17 @@
 function attachEvents() {
-  // TODO:
+  const inputFirstName = document.querySelector('input[name=”firstName”]'); 
+  const inputlLastName = document.querySelector('input[name=”lastName”]'); 
+  const inputFacultyNumber = document.querySelector('input[name=”facultyNumber”]'); 
+  const inputGrade = document.querySelector('input[name=”grade”]'); 
+  const submitBTN = document.getElementById('submit');
+
+
+
+  
+  console.log(inputFirstName.value);
+  console.log(inputlLastName.value);
+  console.log(inputFacultyNumber.value);
+  console.log(inputGrade.value);
 }
 
 attachEvents();
