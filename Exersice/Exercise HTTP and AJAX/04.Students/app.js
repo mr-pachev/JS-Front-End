fd39827eb73b550cd2bd13d0bbfc2483d41c6391
Ivan Pachev <mr.pachev@abv.bg>
2023-12-11 14:18:31 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/04.Students/app.js
+++ Exersice/Exercise HTTP and AJAX/04.Students/app.js
@@ -4,6 +4,7 @@ function attachEvents() {
   const inputFacultyNumber = document.querySelector('input[name="facultyNumber"]'); 
   const inputGrade = document.querySelector('input[name="grade"]'); 
   const submitBtn = document.getElementById('submit');
+  const containerResult = document.querySelector('tbody');
   const BASE_URL = 'http://localhost:3030/jsonstore/collections/students/';
 
   submitBtn.addEventListener('click', async() => {
@@ -20,21 +21,41 @@ function attachEvents() {
       body: JSON.stringify(studentObj)
   })
     .then((res) => res.json())
-    .then(loadStudents)
+    .then(() => {
+      loadStudents()
+    })
   })
 
   async function loadStudents(){
     fetch (BASE_URL)
     .then((res) => res.json())
     .then((studensData) => {
-      const tr = document.createElement('tr');
-      const th = document.createElement('th');
+      
+      const values = Object.values(studensData);
+      
+      for (const obj of values){
+        
+        let tr = document.createElement('tr');
+        let th = document.createEvent('th');
+        // console.log(obj.firstName, obj.lastName, obj.facultyNumber, obj.grade)
 
-      th.textContent += inputFirstName;
-      th.textContent += inputlLastName;
-      th.textContent += inputFacultyNumber;
+        let td = document.createElement('td');
+        td.textContent = obj.firstName;
+        th.appendChild(td);
+        td.textContent = obj.lastName;
+        th.appendChild(td);
+        td.textContent = obj.facultyNumber;
+        th.appendChild(td);
+        td.textContent = obj.grade;
+        th.appendChild(td);
 
-      tr.appendChild(th);
+        tr.appendChild(th);
+        console.log(tr);
+
+        containerResult.appendChild(tr);
+      }
+
+      
     })
   }
   
