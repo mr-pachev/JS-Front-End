ff3a174f9ee9ae475e8cc84ed016a71618dbbdfe
Ivan Pachev <mr.pachev@abv.bg>
2023-12-12 11:46:03 +0200

    Update

--- Exam/Pianist.js
+++ Exam/Pianist.js
@@ -1,9 +1,24 @@
 function solve (input){
     let n = input.shift();
+    let pieceOBj = {};
 
-    confirm.log(input);
+   for (let i = 0; i < n; i++){
+        const [piece, composer, key] = input.shift().split('|');
+        pieceOBj[piece] =  {composer, key};
+    }
+   
+    function addPiece(piece, composer, key){
+
+    }
+
+    function changeKey(piece, newKey){
+
+    }
+
+    function removeePiece(piece){
+
+    }
 }
-
 solve([
     '3',
     'Fur Elise|Beethoven|A Minor',
