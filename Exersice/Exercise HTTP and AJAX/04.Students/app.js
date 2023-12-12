2c3ecd781e8523343c6bf20db27694516361251a
Ivan Pachev <mr.pachev@abv.bg>
2023-12-12 13:55:20 +0200

    Update

--- Exersice/Exercise HTTP and AJAX/04.Students/app.js
+++ Exersice/Exercise HTTP and AJAX/04.Students/app.js
@@ -1,21 +1,21 @@
 function attachEvents() {
   const inputFirstName = document.querySelector('input[name="firstName"]');
   const inputlLastName = document.querySelector('input[name="lastName"]');
-  const inputFacultyNumber = document.querySelector(
-    'input[name="facultyNumber"]'
-  );
+  const inputFacultyNumber = document.querySelector('input[name="facultyNumber"]');
   const inputGrade = document.querySelector('input[name="grade"]');
   const submitBtn = document.getElementById("submit");
   const containerResult = document.querySelector("tbody");
   const BASE_URL = "http://localhost:3030/jsonstore/collections/students/";
 
+  loadStudents();
   submitBtn.addEventListener("click", creatStudent);
 
   function createRow(object) {
     const values = Object.values(object);
     containerResult.innerHTML = "";
 
-    for (const obj of values) {
+      values.forEach((obj) => {
+
       let row = document.createElement("tr");
       row.innerHTML = ` <tr>
                                   <th>${obj.firstName}</th>
@@ -24,7 +24,7 @@ function attachEvents() {
                                   <th>${obj.grade}</th>
                           		</tr>`;
       containerResult.appendChild(row);
-    }
+    });
   }
 
   async function creatStudent() {
@@ -48,16 +48,14 @@ function attachEvents() {
       })
         .then((res) => res.json())
         .then((result) => {
-          creatStudent(result)
           studentObj = {};
           inputFirstName.value = "";
           inputlLastName.value = "";
           inputFacultyNumber.value = "";
           inputGrade.value = "";
+          loadStudents();
         })
         .catch((err) => console.error(err));
-    }else {
-      loadStudents();
     }
   }
 
@@ -68,93 +66,21 @@ function attachEvents() {
         const values = Object.values(studensData);
         containerResult.innerHTML = "";
 
-        for (const obj of values){
-          creatStudent(obj)
-        }
-
-        // for (const obj of values) {
-        //   let row = document.createElement("tr");
-        //   row.innerHTML = ` <tr>
-        //                           <th>${obj.firstName}</th>
-        //                           <th>${obj.lastName}</th>
-        //                           <th>${obj.facultyNumber}</th>
-        //                           <th>${obj.grade}</th>
-        //                   		</tr>`;
-        //   containerResult.appendChild(row);
-
-          // let tr = document.createElement("tr");
-          // let td = document.createElement("td");
-
-          // td.innerHTML = obj.firstName;
-          // tr.appendChild(td);
-
-          // td = document.createElement("td");
-          // td.textContent = obj.lastName;
-          // tr.appendChild(td);
-
-          // td = document.createElement("td");
-          // td.textContent = obj.facultyNumber;
-          // tr.appendChild(td);
-
-          // td = document.createElement("td");
-          // td.textContent = obj.grade;
-          // tr.appendChild(td);
-
-          // containerResult.appendChild(tr);
+        values.forEach((obj) => {
+          let row = document.createElement("tr");
+          row.innerHTML = ` <tr>
+                                  <th>${obj.firstName}</th>
+                                  <th>${obj.lastName}</th>
+                                  <th>${obj.facultyNumber}</th>
+                                  <th>${obj.grade}</th>
+                          		</tr>`;
+      
+          containerResult.appendChild(row);
         
-      })
-      .catch((err) => console.error(err));
+      })})
+      .catch((err) => console.log(err));
   }
-
-  // const issueURL = "http://localhost:3030/jsonstore/collections/students";
-  // const submitButton = document.querySelector("#submit");
-  // const tableBody = document.querySelector("#results tbody");
-
-  // loadStudents();
-
-  // submitButton.addEventListener("click", submitStudent);
-
-  // async function submitStudent() {
-  //   const firstName = document.querySelector("input[name=firstName]").value;
-  //   const lastName = document.querySelector("input[name=lastName]").value;
-  //   const facultyNumber = document.querySelector("input[name=facultyNumber]").value;
-  //   const grade = document.querySelector("input[name=grade]").value;
-
-  //   let sutedntObject = { firstName, lastName, facultyNumber, grade };
-  //   if (firstName && lastName && facultyNumber && grade) {
-  //     await fetch(issueURL, {
-  //       method: "POST",
-  //       body: JSON.stringify(sutedntObject),
-  //     });
-  //   }
-  //   tableBody.textContent = "";
-  //   loadStudents();
-  // }
-  // async function loadStudents() {
-  //   let studentsInfo = await fetch(issueURL);
-  //   let body = await studentsInfo.json();
-  //   let bodyValues = Object.values(body);
-  //   bodyValues.forEach((value) => {
-  //     tableBody.appendChild(createTableRow(value));
-  //   });
-  // }
-
-  // function createTableRow(object) {
-  //   let tr = document.createElement("tr");
-  //   let firstNameBox = document.createElement("td");
-  //   firstNameBox.textContent = object["firstName"];
-  //   let lastNameBox = document.createElement("td");
-  //   lastNameBox.textContent = object["lastName"];
-  //   let facultyNumberBox = document.createElement("td");
-  //   facultyNumberBox.textContent = object["facultyNumber"];
-  //   let gradeBox = document.createElement("td");
-  //   gradeBox.textContent = object["grade"];
-  //   tr.appendChild(firstNameBox);
-  //   tr.appendChild(lastNameBox);
-  //   tr.appendChild(facultyNumberBox);
-  //   tr.appendChild(gradeBox);
-  //   return tr;
-  // }
+ 
 }
 
 attachEvents();
