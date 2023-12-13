29cfbc3009c6859e412f0c6cf37586a3f1621b3a
Ivan Pachev <mr.pachev@abv.bg>
2023-12-13 09:35:43 +0200

    Update

--- Exam/03. Todo-List-Resources/solve.js
+++ Exam/03. Todo-List-Resources/solve.js
@@ -1,6 +1,29 @@
 // TODO
 function attachEvents() {
-  
+  const loadAllBtn = document.getElementById("load-button");
+  const addBtn = document.getElementById("add-button");
+  const textContainer = document.getElementById("title");
+  const BASE_URL = "http://localhost:3030/jsonstore/tasks/";
+
+  loadAllBtn.addEventListener("click", loadItems);
+
+  function loadItems(e) {
+    e.preventDefault();
+
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((itemsObj) => {
+        values = Object.values(itemsObj);
+
+        for (const { name, _id } of values) {
+        }
+      });
+  }
+
+  function creatData (name, _id){
+    const ul = document.getElementById('todo-list');
+    const span = do
+  }
 }
 
 attachEvents();
