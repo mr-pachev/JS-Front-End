66efc825182c973568bc1a9cdc4da6bfd44e250c
Ivan Pachev <mr.pachev@abv.bg>
2023-12-14 11:50:20 +0200

    Update

---
+++ Exam/03. Grocery List_Ресурси/app.js
@@ -0,0 +1,83 @@
+function solve(){
+    let inputFields = {
+        product: document.getElementById('product'),
+        count: document.getElementById('count'),
+        price: document.getElementById('price'),
+    };
+
+    let othersDOMElements = {
+        addBtn: document.getElementById('add-product'),
+        updateBtn: document.getElementById('update-product'),
+        loadBtn: document.getElementById('load-product'),
+        productsContainer: document.getElementById('tbody'),
+    }
+
+    const BASE_URL = 'http://localhost:3030/jsonstore/grocery/';
+    let allProducts = [];
+
+    const {product, count, price} = inputFields;
+    const {addBtn, updateBtn, loadBtn, productsContainer} = othersDOMElements;
+
+    loadBtn.addEventListener('click', loadsProducts);
+
+    function loadsProducts(e){
+        if(e){
+            e.preventDefault();
+        }
+
+        fetch(BASE_URL)
+        .then((res) => res.json())
+        .then((products) => {
+            let values = Object.values(products);
+           for (const {product, count, price} of values) {
+             const row = createElement('tr', productsContainer);
+             const nameProduct = createElement('td', row, product, ['name']);
+             const priceProduct = createElement('td', row, price, ['product-price']);
+           }
+            
+
+        })
+        
+
+    }
+
+    function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
+        const htmlElement = document.createElement(type);
+      
+        if (content && useInnerHtml) {
+          htmlElement.innerHTML = content;
+        } else {
+          if (content && type !== 'input') {
+            htmlElement.textContent = content;
+          }
+      
+          if (content && type === 'input') {
+            htmlElement.value = content;
+          }
+        }
+      
+        if (classes && classes.length > 0) {
+          htmlElement.classList.add(...classes);
+        }
+      
+        if (id) {
+          htmlElement.id = id;
+        }
+      
+        // { src: 'link', href: 'http' }
+        if (attributes) {
+          for (const key in attributes) {
+            htmlElement.setAttribute(key, attributes[key])
+          }
+        }
+      
+        if (parentNode) {
+          parentNode.appendChild(htmlElement);
+        }
+      
+        return htmlElement;
+    }
+
+}
+
+solve();
