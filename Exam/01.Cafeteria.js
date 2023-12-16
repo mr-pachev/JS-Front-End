c9e50c58d1d8ce3a23efca8ef6a8dab3b1dad293
Ivan Pachev <mr.pachev@abv.bg>
2023-12-16 20:02:57 +0200

    Update

--- Exam/01.Cafeteria.js
+++ Exam/01.Cafeteria.js
@@ -8,23 +8,26 @@ function solve(input){
         baristasArr.push({ name, shift, type });  
     }
 
-    while (input.length > 0){
-        const [command, name, shift, type] = input.shift().split(" / ");
+    let inputLine = input.shift().split(" / ");
+    let command = inputLine[0];
+
+    while (command !== 'Closed'){
+        let name = inputLine[1];
         
         const barista = baristasArr.find( barista => barista.name === name ); //проверка дали съществува бариста
         const typeCoffee = barista.type.split(',');
-        const findingType = typeCoffee.find( currentType => currentType === type ); //проверка дали съществява типа кафе
 
         if (!barista){
             return
         }
 
-        if (command === 'Closed'){
-            break;
-        }
-
         switch(command){
-            case 'Prepare':    
+            case 'Prepare':  
+            let shift = inputLine[2];
+            let type = inputLine[3]; 
+            
+            const findingType = typeCoffee.find( currentType => currentType === type ); //проверка дали съществява типа кафе
+
                 if (barista.shift === shift && findingType === type){
                     console.log(`${barista.name} has prepared a ${type} for you!`)
                 }else {
@@ -32,43 +35,57 @@ function solve(input){
                 }
                 break;
             case 'Change Shift':
+                let shiftChange = inputLine[2];
+
                 let isTrue = false;
                 baristasArr.forEach(empl => {
                     if (empl.name === name){
                         isTrue = true;
-                        empl.shift = shift;
+                        empl.shift = shiftChange;
                     }
                   });
                   if (isTrue){
-                      console.log(`${name} has updated his shift to: ${shift}`);
+                      console.log(`${name} has updated his shift to: ${shiftChange}`);
                   }
                 break;
             case 'Learn':
-                if (findingType === type){
-                    console.log(`${barista.name} knows how to make ${type}.`);
+                let newType = inputLine[2];
+                const findingNewType = typeCoffee.find( currentType => currentType === newType );
+                if (findingNewType){
+                    console.log(`${barista.name} knows how to make ${newType}.`);
                 }else {
-                    console.log(``);
+                    baristasArr.forEach(empl => {
+                        if (empl.name === name){
+                            empl.type += `,${newType}`;
+                        }
+                      });
+                    console.log(`${barista.name} has learned a new coffee type: ${newType}.`);
                 }
 
                 break;
         }
 
+        inputLine = input.shift().split(" / ");
+        command = inputLine[0];
     }
+
+    baristasArr.forEach(empl => {
+        let type = empl.type.split(',');
+
+        console.log(`Barista: ${empl.name}, Shift: ${empl.shift}, Drinks: ${type.join(', ')}`)
+      });
+    
 }
 
-solve(['4',
-'Alice day Espresso,Cappuccino',
-'Bob night Latte,Mocha',
-'Carol day Americano,Mocha',
-'David night Espresso',
-'Prepare / Alice / day / Espresso',
-'Change Shift / Bob / day',
-'Learn / Carol / Latte',
-'Prepare / Bob / night / Latte',
-'Learn / David / Cappuccino',
-'Prepare / Carol / day / Cappuccino',
-'Change Shift / Alice / night',
- 'Learn / Bob / Mocha',
-'Prepare / David / night / Espresso',
-'Closed']
+solve([
+    '3',
+      'Alice day Espresso,Cappuccino',
+      'Bob night Latte,Mocha',
+      'Carol day Americano,Mocha',
+      'Prepare / Alice / day / Espresso',
+      'Change Shift / Bob / night',
+      'Learn / Carol / Latte',
+      'Learn / Bob / Latte',
+      'Prepare / Bob / night / Latte',
+      'Closed']
 );
---
+++ Exam/03.Daily-Calorie-Counter_Resources/app.js
@@ -0,0 +1,149 @@
+function solve (){
+    const inputFields = {
+        food: document.getElementById('food'),
+        time: document.getElementById('time'),
+        calories: document.getElementById('calories')
+    }
+
+    const otherDOMElements = {
+        addBtn: document.getElementById('add-meal'),
+        editBtn: document.getElementById('edit-meal'),
+        loadBtn: document.getElementById('load-meals'),
+        mealsContainer: document.getElementById('list'),
+        wrapper: document.getElementById('wrapper'),
+    }
+
+    const {food, time, calories, _id} = inputFields;
+    const {addBtn, editBtn, loadBtn, mealsContainer, wrapper } = otherDOMElements;
+
+    const BASE_URL = 'http://localhost:3030/jsonstore/tasks/';
+
+    const arrMeals = [];
+    const mealObj = {};
+
+    loadBtn.addEventListener('click', loadsMeals);
+
+    function loadsMeals(){
+            fetch(BASE_URL)
+            .then((res) => res.json())
+            .then((data) => {
+                mealsContainer.innerHTML = '';
+                arrMeals.length = 0;
+                arrMeals.push(data);
+            
+                for (const key in data) {
+
+                    const div = createElement("div", mealsContainer, null, [
+                      "meal",
+                    ]);
+                    div.id = data[key]._id;
+                    createElement("h2", div, data[key].food);
+                    createElement("h3", div, data[key].time);
+                    createElement("h3", div, data[key].calories);
+                    const divButtons = createElement("div", div, null, [
+                      "meal-buttons",
+                    ]);
+                    const changeBtn = createElement("button", divButtons, "Change meal", [
+                      "change-meal",
+                    ]);
+                    const deleteBtn = createElement("button", divButtons, "Delete", [
+                      "delete-meal",
+                    ]);
+                    changeBtn.addEventListener('click', chamgeMeal);
+                    deleteBtn.addEventListener('click', (e) => {
+                        const tagId = e.currentTarget.parentNode.parentNode.id;
+                        
+                        const httpHeaders = {
+                            method: 'DELETE'
+                            };
+                    
+                        fetch(`${BASE_URL}${tagId}`, httpHeaders)
+                            .then(() => loadsMeals())
+                            .catch((err) => {
+                                concole.error(err)
+                            })
+                    
+                    })
+                }
+
+                console.log(arrMeals);
+            })
+            .catch(err => console.error(err));
+        }
+
+    function chamgeMeal(e){
+        const tagId = e.currentTarget.parentNode.parentNode.id;
+
+        
+    }
+    
+    addBtn.addEventListener('click', () => {
+            for (const [key, value] of Object.entries(inputFields)) {
+                mealObj[key] = value.value;
+            };
+        
+            const { food, time, calories, _id} = mealObj;
+
+	        const httpHeaders = {
+	        	method: 'POST',
+	        	body: JSON.stringify({ 
+                            food: food.value,
+                            time: time.value,
+                    		calories: calories.value,
+                            id: _id.value
+                })
+	        }
+
+	        fetch(BASE_URL, httpHeaders)
+	        	.then(() => {
+                    loadsMeals();
+                    Object.values(inputFields).forEach((input) => input.value = '');
+                })
+	        	.catch((err) => {
+	        		concole.error(err)
+	        	}
+            )
+        }
+    )
+
+
+    function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
+        const htmlElement = document.createElement(type);
+      
+        if (content && useInnerHtml) {
+          htmlElement.innerHTML = content;
+        } else {
+          if (content && type !== 'input') {
+            htmlElement.textContent = content;
+          }
+      
+          if (content && type === 'input') {
+            htmlElement.value = content;
+          }
+        }
+      
+        if (classes && classes.length > 0) {
+          htmlElement.classList.add(...classes);
+        }
+      
+        if (id) {
+          htmlElement.id = id;
+        }
+      
+        // { src: 'link', href: 'http' }
+        if (attributes) {
+          for (const key in attributes) {
+            htmlElement.setAttribute(key, attributes[key])
+          }
+        }
+      
+        if (parentNode) {
+          parentNode.appendChild(htmlElement);
+        }
+      
+        return htmlElement;
+    }
+
+}
+
+solve();
