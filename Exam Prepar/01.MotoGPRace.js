f493465a7ac272b3b78b1c7c95ee3a6b039133dd
Ivan Pachev <mr.pachev@abv.bg>
2023-12-17 11:16:32 +0200

    Update

--- Exam Prepar/01.MotoGPRace.js
+++ Exam Prepar/01.MotoGPRace.js
@@ -5,8 +5,8 @@ function solve(input) {
     const ridersArr = [];
 
   for (let i = 0; i < n; i++) {
-    const [name, riderInfo] = input.shift().split(" ");                    //сплитване входните данни от всеки ред в масив
-    ridersArr.push({ name, riderInfo });  //всеки масива от реда се добавя, като нов обект в масива
+    const [name, fuel, position] = input.shift().split("|");                    //сплитване входните данни от всеки ред в масив
+    ridersArr.push({ name, fuel: Number(fuel), position: Number(position) });  //всеки масива от реда се добавя, като нов обект в масива
 
   }
 
@@ -14,38 +14,86 @@ function solve(input) {
   let command = inputLine[0];
 
   while(command !== 'Finish'){
-    const name = inputLine[1]
+    const riderName = inputLine[1]
 
-    for (const index of ridersArr) {
-      console.log(index)  ;
-    }
-
-    // const rider = ridersArr.find(obj => obj.name === name ); //проверка дали съществува бариста
-    // // const [ riderName, fuel, position ] = rider.riderInfo.split('|'); 
+    const rider = ridersArr.find(obj => obj.name === riderName ); //проверка дали съществува бариста
 
     if(!rider){
         return;
     }
-
 
     switch(command){
         case 'StopForFuel':
         let minimumFuel = inputLine[2];
         let changePositon = inputLine[3];
        
+        if (minimumFuel > rider.fuel){
+          for (const obj of ridersArr) {
+            if(obj.name === riderName){
+              obj.position = changePositon;
+            }
+          }
+          console.log(`${riderName} stopped to refuel but lost his position, now he is ${changePositon}.`)
+        }else {
+          for (const obj of ridersArr) {
+            if(obj.name === riderName){
+              obj.fuel -= minimumFuel;
+            }
+          }
 
+          console.log(`${riderName} does not need to stop for fuel!`);
+        }
             break;
-
-
 
         case 'Overtaking':
+        let rideOneName = inputLine[1];
+        let rideTwoName = inputLine[2];
+
+        let rideOnePosition = findByName(rideOneName).position;
+        let rideTwoPosition = findByName(rideTwoName).position;
+
+        for (const obj of ridersArr) {
+          if(obj.name === rideOneName){
+            obj.position = rideTwoPosition
+          }
+        }
+        for (const obj of ridersArr) {
+          if(obj.name === rideTwoName){
+            obj.position = rideOnePosition
+          }
+        }
+
+        if (rideOnePosition > rideTwoPosition){
+          console.log(`${rideOneName} overtook ${rideTwoName}!`);
+        }
             break;
+
         case 'EngineFail':
+        let riderEngineFailName = inputLine[1];
+        let lapsLeft = inputLine[2];
+
+        let index = ridersArr.indexOf(findByName(riderEngineFailName));
+	        ridersArr.splice(index, 1);
+
+        console.log(`${riderEngineFailName} is out of the race because of a technical issue, ${lapsLeft} laps before the finish.`);
             break;
     }
 
     inputLine = input.shift().split(' - ');
     command = inputLine[0];
+  }
+  
+  ridersArr.forEach(rider => {
+    console.log(`${rider.name}:\nFinal position: ${rider.position}`);
+  });
+
+
+  function findByName(riderName){
+    for (const obj of ridersArr) {
+      if(obj.name === riderName){
+        return obj
+      }
+    }
   }
 }
 
