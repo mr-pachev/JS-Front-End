541e03f2331ad89c80a8425494fcf9a256d76b58
Ivan Pachev <mr.pachev@abv.bg>
2023-12-17 14:28:07 +0200

    Update

--- Exam Prepar/astroAdventure.js
+++ Exam Prepar/astroAdventure.js
@@ -1,120 +1,90 @@
 function solve(input) {
   const n = input.shift();
 
-  let astronauts = [];
+  let astronautsArr = [];
 
   for (let i = 0; i < n; i++) {
     const [name, oxygen, energy] = input.shift().split(" ");                    //сплитване входните данни от всеки ред в масив
-    astronauts.push({ name, oxygen: Number(oxygen), energy: Number(energy) });  //всеки масива от реда се добавя, като нов обект в масива
+    astronautsArr.push({ name, oxygen: Number(oxygen), energy: Number(energy) });  //всеки масива от реда се добавя, като нов обект в масива
   }
 
-  let inputLine = input.shift();                      //взимане на следващия ред
-  let command = inputLine.split(" - ")[0];            //сплитва реда, като елементи с индекси и взима командата
+   let inputLine = input.shift().split(' - ');
+   let command = inputLine[0];
 
-  while (command !== "End") {
-    let name = inputLine.split(" - ")[1];             //името
+   while(command !== 'End'){
+    let astroName = inputLine[1];
 
-    if (command === "Explore") {
-      const neededEnergy = inputLine.split(" - ")[2]; //нужната енергия за извършване на работата
-      explore(name, neededEnergy);
+    const astronaut = astronautsArr.find( astr => astr.name === astroName );
 
-    } else if (command === "Refuel") {
-      const refAmount = inputLine.split(" - ")[2];    //нова енергия за астронавта
-      refuel(name, refAmount);
-
-    } else if (command === "Breathe") {
-      const refOxygen = inputLine.split(" - ")[2];    //допълнително кислород за астронавта
-      breathe(name, refOxygen);
+    if(!astronaut){
+      return;
     }
 
-    inputLine = input.shift();                        //следващия реда с входни данни
-    command = inputLine.split(" - ")[0];              //новата команда
-  }
-
-  //проверка наличието на енергия за извършване на задачата
-  function explore(nameAstro, neededEnergy) {       
-    for (const key in astronauts) {
-      if (astronauts[key].name === nameAstro) {
-
-        if (astronauts[key].energy >= neededEnergy) {
-          astronauts[key].energy -= neededEnergy;
-          console.log(
-            `${nameAstro} has successfully explored a new area and now has ${astronauts[key].energy} energy!`
-          );
-        } else {
-          console.log(`${nameAstro} does not have enough energy to explore!`);
+    switch(command){
+      case 'Explore':
+        let energyNeeded = inputLine[2];
+  
+        for (const obj of astronautsArr) {
+          if(obj.name === astroName){
+            
+            if (obj.energy >= energyNeeded){
+              obj.energy -= energyNeeded;
+             
+              console.log(`${astroName} has successfully explored a new area and now has ${obj.energy} energy!`);
+            }else {
+              console.log(`${astroName} does not have enough energy to explore!`);
+            }
+          }
         }
-      }
-    }
-  }
+ 
+        break;
 
-  //зареждане с допълнителна енергия
-  function refuel(nameAstro, refAmount) {
-    for (const key in astronauts) {
-      if (astronauts[key].name === nameAstro) {
-        let result = astronauts[key].energy + Number(refAmount);
+      case 'Refuel':
+        let amountRef = inputLine[2];
 
-        if (result > 200) {
-          console.log(
-            `${nameAstro} refueled their energy by ${
-              200 - astronauts[key].energy
-            }!`
-          );
-          astronauts[key].energy = 200;
-        } else {
-          astronauts[key].energy = result;
-          console.log(`${nameAstro} refueled their energy by ${refAmount}!`);
+        for (const obj of astronautsArr) {
+          if(obj.name === astroName){
+            const energyRecoverd = Math.min(amountRef, 200 - obj.energy);
+
+            obj.energy += energyRecoverd;
+
+            console.log(`${astroName} refueled their energy by ${energyRecoverd}!`);
+          }
         }
-      }
-    }
-  }
+        break;
 
-  //зареждане с допълнителен кислород
-  function breathe(nameAstro, refOxygen) {
-    for (const key in astronauts) {
-      if (astronauts[key].name === nameAstro) {
-        let result = astronauts[key].oxygen + Number(refOxygen);
+      case 'Breathe':
+        let amountBreathe = inputLine[2];
 
-        if (result >= 100) {
-          console.log(
-            `${nameAstro} took a breath and recovered ${
-              100 - astronauts[key].oxygen
-            } oxygen!`
-          );
-          astronauts[key].oxygen = 100;
-        } else {
-          astronauts[key].oxygen = result;
-          console.log(
-            `${nameAstro} took a breath and recovered ${result} oxygen!`
-          );
+        for (const obj of astronautsArr) {
+          if(obj.name === astroName){
+            const oxygeRecoverd = Math.min(amountBreathe, 100 - obj.oxygen);
+
+            obj.oxygen += oxygeRecoverd;
+
+            console.log(`${astroName} took a breath and recovered ${oxygeRecoverd} oxygen!`);
+          }
         }
-      }
+        break;
     }
-  }
 
-  astronauts.forEach(astronaut => {
+    inputLine = input.shift().split(' - ');
+    command = inputLine[0];
+   }
+
+   astronautsArr.forEach(astronaut => {
     console.log(`Astronaut: ${astronaut.name}, Oxygen: ${astronaut.oxygen}, Energy: ${astronaut.energy}`);
   });
 
-//   for (const key in astronauts) {
-//     console.log(
-//       `Astronaut: ${astronauts[key].name}, Oxygen: ${astronauts[key].oxygen}, Energy: ${astronauts[key].energy}`
-//     );
-//   }
 }
 
-solve([
-  "4",
-  "Alice 60 100",
-  "Bob 40 80",
-  "Charlie 70 150",
-  "Dave 80 180",
-  "Explore - Bob - 60",
-  "Refuel - Alice - 30",
-  "Breathe - Charlie - 50",
-  "Refuel - Dave - 40",
-  "Explore - Bob - 40",
-  "Breathe - Charlie - 30",
-  "Explore - Alice - 40",
-  "End",
-]);
+solve(['4',
+'Alice 60 100',
+'Bob 40 80',
+'Charlie 70 150',
+'Dave 80 180',
+'Explore - Bob - 60',
+'Refuel - Alice - 30',
+'Breathe - Charlie - 50',
+'End']
+);
