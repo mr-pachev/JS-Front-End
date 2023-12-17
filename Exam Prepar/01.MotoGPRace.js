5c8cb48db535a9ebeddaece5738c57defca9f4d7
Ivan Pachev <mr.pachev@abv.bg>
2023-12-17 10:09:04 +0200

    Update

--- Exam Prepar/01.MotoGPRace.js
+++ Exam Prepar/01.MotoGPRace.js
@@ -2,15 +2,51 @@ function solve(input) {
 
     let n = input.shift();
 
-    let ridersArr = [];
+    const ridersArr = [];
 
   for (let i = 0; i < n; i++) {
     const [name, riderInfo] = input.shift().split(" ");                    //сплитване входните данни от всеки ред в масив
-    ridersArr.push({ name, riderInfo: Number(riderInfo.split('|')) });  //всеки масива от реда се добавя, като нов обект в масива
+    ridersArr.push({ name, riderInfo });  //всеки масива от реда се добавя, като нов обект в масива
 
   }
 
-  console.log(ridersArr)
+  let inputLine = input.shift().split(' - ');
+  let command = inputLine[0];
+
+  while(command !== 'Finish'){
+    const name = inputLine[1]
+
+    for (const index of ridersArr) {
+      console.log(index)  ;
+    }
+
+    // const rider = ridersArr.find(obj => obj.name === name ); //проверка дали съществува бариста
+    // // const [ riderName, fuel, position ] = rider.riderInfo.split('|'); 
+
+    if(!rider){
+        return;
+    }
+
+
+    switch(command){
+        case 'StopForFuel':
+        let minimumFuel = inputLine[2];
+        let changePositon = inputLine[3];
+       
+
+            break;
+
+
+
+        case 'Overtaking':
+            break;
+        case 'EngineFail':
+            break;
+    }
+
+    inputLine = input.shift().split(' - ');
+    command = inputLine[0];
+  }
 }
 
 solve([
