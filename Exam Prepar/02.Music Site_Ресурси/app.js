545ee6aad1650ffe74129291c9d1a3dca3fec284
Ivan Pachev <mr.pachev@abv.bg>
2023-12-18 18:56:34 +0200

    Update

---
+++ Exam Prepar/02.Music Site_Ресурси/app.js
@@ -0,0 +1,130 @@
+window.addEventListener("load", solve);
+
+function solve() {
+  let countLikes = 0;
+  const inputFileds = {
+    genre: document.getElementById("genre"),
+    name: document.getElementById("name"),
+    author: document.getElementById("author"),
+    date: document.getElementById("date"),
+  };
+
+  const othersDOMElements = {
+    addBtn: document.getElementById("add-btn"),
+    collectionsSong: document.querySelector(".all-hits-container"),
+    saveSongsContainer: document.querySelector(".saved-container"),
+    likesContainer: document.querySelector(".likes > p"),
+  };
+
+  const { addBtn, collectionsSong, saveSongsContainer, likesContainer } =
+    othersDOMElements;
+  const { genre, name, author, date } = inputFileds;
+
+  addBtn.addEventListener("click", createSong);
+
+  function createSong(e) {
+    e.preventDefault();
+
+    //проверка за непопълнени полета
+    let allFormInputs = Object.values(inputFileds).every(
+      (input) => input.value !== ""
+    );
+
+    if (!allFormInputs) {
+      return;
+    }
+
+    //създаване на DOM елементите
+    const divContainer = createElement("div", collectionsSong, null, [
+      "hits-info",
+    ]);
+    const img = createElement("img", divContainer, null, null, null, {
+      src: "./static/img/img.png",
+    });
+    const h2Gener = createElement("h2", divContainer, `Genre: ${genre.value}`);
+    const h2Name = createElement("h2", divContainer, `Name: ${name.value}`);
+    const h2Author = createElement(
+      "h2",
+      divContainer,
+      `Author: ${author.value}`
+    );
+    const h3Date = createElement("h3", divContainer, `Date: ${date.value}`);
+    const saveBtn = createElement("button", divContainer, "Save song", [
+      "save-btn",
+    ]);
+    const likeBtn = createElement("button", divContainer, "Like song", [
+      "like-btn",
+    ]);
+    const deleteBtn = createElement("button", divContainer, "Delete", [
+      "delete-btn",
+    ]);
+
+    saveBtn.addEventListener("click", saveSongs);
+    likeBtn.addEventListener("click", likeSong);
+    deleteBtn.addEventListener("click", deleteSong);
+
+    Object.values(inputFileds).forEach((input) => input.value = '');
+  }
+
+  function saveSongs(e) {
+    const parentElement = e.currentTarget.parentNode;
+    saveSongsContainer.appendChild(parentElement);
+    parentElement.querySelector(".save-btn").remove();
+    parentElement.querySelector(".like-btn").remove();
+  }
+
+  function likeSong(e) {
+    countLikes++;
+    likesContainer.textContent = `Total Likes: ${countLikes}`;
+    e.currentTarget.disabled = true;
+  }
+
+  function deleteSong(e) {
+    e.currentTarget.parentNode.remove();
+  }
+
+  function createElement(
+    type,
+    parentNode,
+    content,
+    classes,
+    id,
+    attributes,
+    useInnerHtml
+  ) {
+    const htmlElement = document.createElement(type);
+
+    if (content && useInnerHtml) {
+      htmlElement.innerHTML = content;
+    } else {
+      if (content && type !== "input") {
+        htmlElement.textContent = content;
+      }
+
+      if (content && type === "input") {
+        htmlElement.value = content;
+      }
+    }
+
+    if (classes && classes.length > 0) {
+      htmlElement.classList.add(...classes);
+    }
+
+    if (id) {
+      htmlElement.id = id;
+    }
+
+    // { src: 'link', href: 'http' }
+    if (attributes) {
+      for (const key in attributes) {
+        htmlElement.setAttribute(key, attributes[key]);
+      }
+    }
+
+    if (parentNode) {
+      parentNode.appendChild(htmlElement);
+    }
+
+    return htmlElement;
+  }
+}
