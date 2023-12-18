7a44c17775a99c0426da4467cacb1f617c52d464
Ivan Pachev <mr.pachev@abv.bg>
2023-12-18 11:36:24 +0200

    Update

--- Exam Prepar/01.Horse Racing.js
+++ Exam Prepar/01.Horse Racing.js
@@ -6,6 +6,8 @@ function solve(input){
     for (const hourse of lineInput.split('|')) {
         hourseArr.push(hourse);
     }
+ 
+    hourseArr.reverse();
 
     lineInput = input.shift().split(' ');
     let command = lineInput[0];
@@ -20,54 +22,69 @@ function solve(input){
                 let indexOne = hourseArr.indexOf(overtakingHorse);
                 let indexTwo = hourseArr.indexOf(overtakenHorse);
 
-                const temp = hourseArr[indexOne];
-                hourseArr[indexOne] = hourseArr[indexTwo];
-                hourseArr[indexTwo] = temp;
-
-                console.log(`'${overtakingHorse} retakes ${overtakenHorse}.`)
+                if(indexOne > indexTwo){
+                    hourseArr.splice(indexOne, 1);
+                    hourseArr.splice(indexTwo + 1, 0, overtakingHorse);
+    
+                    console.log(`${overtakingHorse} retakes ${overtakenHorse}.`)
+                }
                 break;
 
             case 'Trouble':
                 let horseName = lineInput[1];
                 let firstIndex = hourseArr.indexOf(horseName);
-                let secondIndex = firstIndex + 1;
 
-                if (firstIndex < horseName.length-1){
-                    const temporary = hourseArr[firstIndex];
-                    hourseArr[firstIndex] = hourseArr[secondIndex];
-                    hourseArr[secondIndex] = temporary;
+                if (firstIndex < hourseArr.length-1){
+                    hourseArr.splice(firstIndex, 1);
+                    hourseArr.splice(firstIndex + 1, 0, horseName);
+                    console.log(`Trouble for ${horseName} - drops one position.`);
                 }
              
-                console.log(`Trouble for ${horseName} - drops one position.`);
                 break;
 
             case 'Rage':
                 let rageName = lineInput[1];
                 let firstInd = hourseArr.indexOf(rageName);
-                let secondInd = firstIndex + 1;
 
-                if (firstIndex < horseName.length-1){
-                    const temporary = hourseArr[firstInd];
-                    hourseArr[firstInd] = hourseArr[secondInd];
-                    hourseArr[secondInd] = temporary;
+                if (firstInd > 1) {
+                    hourseArr.splice(firstInd, 1);
+                    hourseArr.splice(firstInd - 2, 0, rageName);
+                }else if (firstInd === 1) {
+                    hourseArr.splice(firstInd, 1);
+                    hourseArr.splice(firstInd - 1, 0, rageName);
                 }
 
+                console.log(`${rageName} rages 2 positions ahead.`);
                 break;
+
             case 'Miracle':
+                let miracleName = hourseArr[hourseArr.length-1];
+
+                hourseArr.splice(hourseArr.length-1, 1);
+                hourseArr.splice(0, 0, miracleName);
+
+                let endedEl = hourseArr[1];
+                hourseArr.splice(1, 1);
+                hourseArr.splice(hourseArr.length, 0, endedEl);
+
+
+                console.log(`What a miracle - ${miracleName} becomes first.`);
                 break;
         }
-
 
         lineInput = input.shift().split(' ');
         command = lineInput[0];
     }
+    hourseArr.reverse();
 
+    console.log(hourseArr.join('->'));
+    console.log(`The winner is: ${hourseArr[hourseArr.length-1]}`)
 }
 
-solve((['Onyx|Domino|Sugar|Fiona',
-'Trouble Onyx',
-'Retake Onyx Sugar',
-'Rage Domino',
-'Miracle',
+solve(['Bella|Alexia|Sugar',
+'Retake Alexia Sugar',
+'Rage Bella',
+'Trouble Bella',
 'Finish'])
-);
+
+
