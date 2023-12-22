a73df1c48d6c2cd16139e945eb9ec0fc54d9654f
Ivan Pachev <mr.pachev@abv.bg>
2023-12-22 10:48:50 +0200

    Update

--- Exam/01.Encoded Cryptocurrency.js
+++ Exam/01.Encoded Cryptocurrency.js
@@ -1,5 +1,51 @@
 function solve(input){
-    
+    let chartersArr = input.shift().split('');
+
+    let inputLine = input.shift().split('?');
+    let command = inputLine[0];
+
+    while(command !== 'Buy'){
+
+      switch(command){
+        case 'TakeEven':
+            let bufferArr = [];
+            for (let i = 0; i < chartersArr.length; i++){
+                if (i % 2 === 0){
+                    bufferArr.push(chartersArr[i]);
+                }
+            }
+
+            chartersArr.length = 0;
+            chartersArr = bufferArr;
+
+            console.log(chartersArr.join(''));
+          break;
+        case 'ChangeAll':
+            let substring = inputLine[1];
+            let replacement = inputLine[2];
+            
+            for (let i = 0; i < chartersArr.length; i++){
+                if (chartersArr[i] === substring){
+                    chartersArr[i] = replacement;
+                }
+            }
+
+            console.log(chartersArr.join(''));
+          break;
+        case 'Reverse':
+            let ReverseeKey = inputLine[1];
+
+            let arrToString = chartersArr.join('') + '';
+            let findingSubstr = arrToString.find(p => p === ReverseeKey);
+
+            console.log()
+          break;
+      }
+
+      inputLine = input.shift().split('?');
+      command = inputLine[0];
+    }
+
 }
 
 solve((["PZDfA2PkAsakhnefZ7aZ", 
